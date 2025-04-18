2ac3a25687a374010cb2c965b584ed10f31c7f39
Kruziikrel13 <dev@michaelpetersen.io>
2025-04-18 13:23:20 +1000

    update comments

--- modules/gaming.nix
+++ modules/gaming.nix
@@ -7,6 +7,9 @@ let cfg = config.opts; in
       enable = mkEnableOption null;
       diskUuid = mkOption {
         type = str;
+        description = ''
+          Gaming Drive Disk UUID to mount
+        '';
       };
     };
   };
@@ -20,7 +23,7 @@ let cfg = config.opts; in
     };
 
     ## Set the below when using a Keychron keyboard to ensure that steam doesn't think it's a controller.
-    ### I used this specifically to fix input bugs with Metal Gear Solid 5
+    # This specifically fixes a major input bug when playing MGSV however it's likely it'll have benefits elsewhere when gaming.
     services.udev.extraRules = ''
       SUBSYSTEM=="input", ATTRS{idVendor}=="3434", ATTRS{idProduct}=="0161", ENV{ID_INPUT_JOYSTICK}="" 
     '';
@@ -39,8 +42,7 @@ let cfg = config.opts; in
         gamescopeSession.enable = true;
         protontricks.enable = true;
         extraCompatPackages = [ pkgs.proton-ge-bin ];
-        ## Currently seems to cause steam logs to spam missing libs
-        # extest.enable = true;
+        # extest.enable = true; ## Currently seems to cause steam logs to spam missing extest 32bit libs
       };
       gamemode.enable = true;
     };
