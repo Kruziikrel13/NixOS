9a9a48c9d6d48b4ba0d155bf8494e414fc3d363d
Kruziikrel13 <dev@michaelpetersen.io>
2025-04-29 15:28:36 +1000

    further bash configuration

--- modules/home/shell.nix
+++ modules/home/shell.nix
@@ -3,6 +3,15 @@
   home.shell.enableBashIntegration = true;
   programs.bash = {
     enable = true;
+    enableCompletion = true;
+    enableVteIntegration = true;
+    shellAliases = {
+      home-edit = "cd /etc/nixos/modules/home && nvim && cd -";
+      nix-edit = "cd /etc/nixos && nvim && cd -";
+      gc = "nix-collect-garbage -d";
+      sgc = "sudo nix-collect-garbage -d";
+      srb = "sudo nixos-rebuild switch";
+    };
   };
   programs.direnv = {
     enable = true;
