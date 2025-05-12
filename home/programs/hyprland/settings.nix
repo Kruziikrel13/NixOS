f062dff0e137cd341563d3717eb4b407ce38599d
Kruziikrel13 <dev@michaelpetersen.io>
2025-05-12 19:02:47 +1000

    add workspace and monitor config

--- home/programs/hyprland/settings.nix
+++ home/programs/hyprland/settings.nix
@@ -4,6 +4,20 @@
       "$terminal" = "ghostty";
       "$dmenu" = "walker --modules applications";
       "$mainMod" = "SUPER";
+      
+      monitor = [
+	      "desc:GIGA-BYTE TECHNOLOGY CO. LTD. AORUS FI32U 21440B000115,3840x2160@144.0,1920x0,1.0"
+      "desc:ViewSonic Corporation VX2758-C-MH V9M184500179,1920x1080@60.0,5760x849,1.0"
+      "desc:AOC 24G1WG4 0x0004A33C,1920x1080@144.0,0x344,1.0"
+      ];
+
+      workspace = [
+      "1,monitor:desc:AOC 24G1WG4 0x0004A33C,default:true"
+"2,monitor:desc:GIGA-BYTE TECHNOLOGY CO. LTD. AORUS FI32U 21440B000115,default:true"
+"3,monitor:desc:ViewSonic Corporation VX2758-C-MH V9M184500179,default:true"
+"10,monitor:desc:AOC 24G1WG4 0x0004A33C"
+"9,monitor:desc:ViewSonic Corporation VX2758-C-MH V9M184500179"
+      ];
 
       env = [
         "XCURSOR_SIZE,32"
