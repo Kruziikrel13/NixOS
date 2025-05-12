27ed0378a1c9d00311d2b9253384109e6514c2cf
Kruziikrel13 <dev@michaelpetersen.io>
2025-05-12 20:59:28 +1000

    remove duplicate nvim

--- home/.config/nvim/init.lua
+++
@@ -1,9 +0,0 @@
-vim.uv = vim.uv or vim.loop
-vim.loader.enable()
-
-vim.g.mapleader = require('configuration').mapleader
-vim.g.maplocalleader = require('configuration').maplocalleader
-
-require('init').plugin_setup()
-
-vim.lsp.enable({'nixd', 'lua_ls', 'ts_ls'})
