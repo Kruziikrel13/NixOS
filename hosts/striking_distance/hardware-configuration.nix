dd7c98f71276a5072cb4333a7da19aadeccd6f57
Kruziikrel13 <dev@michaelpetersen.io>
2025-05-13 01:19:14 +1000

    add gaming drive mount opts for (hopefully) better performance

--- hosts/striking_distance/hardware-configuration.nix
+++ hosts/striking_distance/hardware-configuration.nix
@@ -27,6 +27,7 @@
   fileSystems."/home/kruziikrel13/games" =
     { device = "/dev/disk/by-uuid/4bfedbcc-6059-4ff5-aa86-c5d49ee1a9d0";
       fsType = "ext4";
+      options = [ "defaults" "noatime" "barrier=1" "nofail" ];
     };
 
   swapDevices = [ ];
