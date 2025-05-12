8c32ca76978b15f83299f5979530e7d57858fd46
Kruziikrel13 <dev@michaelpetersen.io>
2025-05-13 01:19:37 +1000

    use cpu-freq and pulseaudio on laptop

--- hosts/atlas/default.nix
+++ hosts/atlas/default.nix
@@ -8,10 +8,13 @@
 
   services.libinput.enable = true;
   boot.kernelPackages = pkgs.linuxPackages_latest;
+  services.auto-cpufreq.enable = true;
 
   hardware.bluetooth = {
     enable = true;
     powerOnBoot = true;
   };
   services.blueman.enable = true;
+
+  services.pulseaudio.enable = true;
 }
