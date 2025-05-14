a3c51261f21f01ed7540c5189dc696a4f77c08b6
Kruziikrel13 <dev@michaelpetersen.io>
2025-05-15 09:52:49 +1000

    various attempts to get proton fixes working on steam

--- hosts/striking_distance/home/gaming.nix
+++ hosts/striking_distance/home/gaming.nix
@@ -1,8 +1,8 @@
-{pkgs, ...}: {
+{pkgs, config, ...}: {
+  home.sessionVariables.WINEPREFIX = "${config.xdg.dataHome}/wine";
   home.packages = with pkgs; [ 
     wineWowPackages.wayland 
     gamescope
     winetricks
-    protontricks
   ];
 }
