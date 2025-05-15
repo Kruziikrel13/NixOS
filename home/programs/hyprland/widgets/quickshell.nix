c90071d4143d49ad6dca3572ac72ecdf4e3a7360
Kruziikrel13 <dev@michaelpetersen.io>
2025-05-15 10:26:25 +1000

    populate quickshell widget

--- home/programs/hyprland/widgets/quickshell.nix
+++ home/programs/hyprland/widgets/quickshell.nix
@@ -1,4 +1,10 @@
-{ lib, config, ... }: let
+{ 
+  lib, 
+  config, 
+  inputs, 
+  pkgs, 
+  ... 
+}: let
   cfg = config.programs.quickshell;
 in {
   options = {
@@ -15,5 +21,28 @@ in {
         message = "Quickshell widget conflicts with AGS widget. Only enable one.";
       }
     ];
+    home.packages = [ inputs.quickshell.packages.${pkgs.system}.default ];
+    xdg.configFile.quickshell = {
+      enable = true;
+      source = config.lib.file.mkOutOfStoreSymlink /etc/nixos/config/quickshell;
+    };
+    systemd.user.services = lib.mkIf cfg.systemd.enable {
+      quickshell = {
+        Unit = {
+          Description = "Quickshell Desktop Widget";
+          PartOf = [ config.wayland.systemd.target ];
+          Requires = [ config.wayland.systemd.target ];
+          After = [ config.wayland.systemd.target "tray.target" ];
+        };
+
+        Service = {
+          ExecStart = "${inputs.quickshell.packages.${pkgs.system}.default}/bin/quickshell";
+          Restart = "on-failure";
+          KillMode = "mixed";
+        };
+
+        Install.WantedBy = [ config.wayland.systemd.target ];
+      };
+    };
   };
 }
