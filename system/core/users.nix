04ded0b3e92abc7e1b81410fcda01fd9ce90e578
Kruziikrel13 <dev@michaelpetersen.io>
2025-05-15 10:00:01 +1000

    add user to gamemode group

--- system/core/users.nix
+++ system/core/users.nix
@@ -4,6 +4,7 @@
     description = "Primary user account for system";
     hashedPassword = "$7$CU..../....kUbGCK9CWgYVn7po7zdyz0$CxvNCDMqGFzOZBAO0iAhytSnonc.LuMyvv1FodplVaB";
     extraGroups = [
+      "gamemode"
       "input"
       "audio"
       "networkmanager"
