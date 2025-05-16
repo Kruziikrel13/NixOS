96d5d0a31263173b4afc5981bd3fb040e5b20ca3
Kruziikrel13 <dev@michaelpetersen.io>
2025-05-17 08:19:20 +1000

    added pycharm

--- home/editors/jetbrains.nix
+++ home/editors/jetbrains.nix
@@ -1,6 +1,7 @@
 {pkgs, ...}: let
   idea = pkgs.jetbrains.idea-ultimate;
+  pycharm = pkgs.jetbrains.pycharm-professional;
 in {
-  home.packages = [ idea ];
+  home.packages = [ idea pycharm ];
   xdg.configFile."JetBrains/IntelliJIdea${idea.version}/idea64.vmoptions".text = "-Dawt.toolkit.name=WLToolkit";
 }
