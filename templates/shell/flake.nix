b017c66345d5994d287e6e31d0c56d3b2255c60f
Kruziikrel13 <dev@michaelpetersen.io>
2025-05-20 19:38:31 +1000

    fixed up flake devshell template

--- templates/shell/flake.nix
+++ templates/shell/flake.nix
@@ -3,16 +3,14 @@
 
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
-  outputs = { nixpkgs }: let
+  outputs = { nixpkgs, ... }: let
     system = "x86_64-linux";
-    pkgs = import nixpkgs { inherit system; };
+    pkgs = import nixpkgs {inherit system;};
   in {
-    devShells.default = pkgs.mkShell {
-      packages = with pkgs; [ ];
-      env = { };
-      shellHook = ''
-
-      '';
+    devShells = {
+      ${system}.default = pkgs.mkShell {
+        packages = with pkgs; [ ];
+      };
     };
   };
 }
