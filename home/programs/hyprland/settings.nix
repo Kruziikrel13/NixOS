46e16a421f49848bd3d4c4656c87832f1853f9c8
Kruziikrel13 <dev@michaelpetersen.io>
2025-05-23 09:31:26 +1000

    games should inhibit idle (fixed hopefully)

--- home/programs/hyprland/settings.nix
+++ home/programs/hyprland/settings.nix
@@ -123,7 +123,7 @@ in {
 
       windowrulev2 = [
         "suppressevent maximize, class:.*"
-        "idleinhibit fullscreen, class:^steam_app_*"
+        "idleinhibit fullscreen, class:steam_app_*"
         "idleinhibit fullscreen, class:firefox"
         "nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"
       ];
