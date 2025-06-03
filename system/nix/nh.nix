7ce0026a50a8d62cff3b0c9302cadc8ea42b2e71
Kruziikrel13 <dev@michaelpetersen.io>
2025-06-03 13:22:54 +1000

    system(nix): don't sleep before editing nixos config

--- system/nix/nh.nix
+++ system/nix/nh.nix
@@ -6,7 +6,7 @@
   programs.nh.enable = true;
 
   environment.shellAliases = {
-    nixos-edit = "cd ${root}; sleep 1; nvim; cd -";
+    nixos-edit = "cd ${root}; nvim; cd -";
     nixos-build = "${config.programs.nh.package}/bin/nh os switch ${root}";
     nixos-upgrade = "${config.programs.nh.package}/bin/nh os switch ${root} --update";
     nixos-clean = "${config.programs.nh.package}/bin/nh clean all --nogcroots";
