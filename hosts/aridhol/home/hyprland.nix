59467a5136e5c073d2cbee27fdbce777cf6452f1
Kruziikrel13 <dev@michaelpetersen.io>
2025-06-11 10:14:01 +1000

    aridhol(hyprland): remove redundant workspace rules

--- hosts/aridhol/home/hyprland.nix
+++ hosts/aridhol/home/hyprland.nix
@@ -7,18 +7,6 @@
     '';
   };
 
-  xdg.configFile.workspaces = {
-    enable = true;
-    target = "hypr/workspaces.conf";
-    text = ''
-      workspace=1,monitor:desc:AOC 24G1WG4 0x0004A33C,default:true
-      workspace=2,monitor:desc:GIGA-BYTE TECHNOLOGY CO. LTD. AORUS FI32U 21440B000115,default:true
-      workspace=3,monitor:desc:ViewSonic Corporation VX2758-C-MH V9M184500179,default:true
-      workspace=10,monitor:desc:AOC 24G1WG4 0x0004A33C
-      workspace=9,monitor:desc:ViewSonic Corporation VX2758-C-MH V9M184500179
-    '';
-  };
-
   wayland.windowManager.hyprland = {
     sourceFirst = true;
     settings = {
