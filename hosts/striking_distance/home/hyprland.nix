4aec7478d3ea800185c7dde7931a4ea2cde74686
Kruziikrel13 <dev@michaelpetersen.io>
2025-06-13 11:31:06 +1000

    striking-distance(hyprland): lower monitor refresh rate
    to avoid strange visual glitches

--- hosts/striking_distance/home/hyprland.nix
+++ hosts/striking_distance/home/hyprland.nix
@@ -5,7 +5,7 @@
     text = ''
       monitor=desc:GIGA-BYTE TECHNOLOGY CO. LTD. AORUS FI32U 21440B000115,3840x2160@144.0,1920x0,1.0
       monitor=desc:ViewSonic Corporation VX2758-C-MH V9M184500179,1920x1080@60.0,5760x849,1.0
-      monitor=desc:AOC 24G1WG4 0x0004A33C,1920x1080@144.0,0x344,1.0
+      monitor=desc:AOC 24G1WG4 0x0004A33C,1920x1080@60.0,0x344,1.0
     '';
   };
 
