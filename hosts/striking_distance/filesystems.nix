855f48bee2c655bae9cd306b3c1cc01a8c3984ca
kruziikrel13 <dev@michaelpetersen.io>
2025-06-18 22:36:00 +1000

    striking-distance: mount games drive by label

--- hosts/striking_distance/filesystems.nix
+++ hosts/striking_distance/filesystems.nix
@@ -1,7 +1,7 @@
 {username, ...}: {
   fileSystems = {
     "/home/${username}/games" = {
-      device = "/dev/disk/by-uuid/4bfedbcc-6059-4ff5-aa86-c5d49ee1a9d0";
+      device = "/dev/disk/by-label/games";
       fsType = "ext4";
       options = ["defaults" "noatime" "barrier=1" "nofail"];
     };
