c04fa0740b16928c953d5149f42bf4974ad287ff
kruziikrel13 <dev@michaelpetersen.io>
2025-07-09 12:12:13 +1000

    home(hyprland.rules): re-organized rules layout and added some additional xwayland rules

--- home/programs/hyprland/rules.nix
+++ home/programs/hyprland/rules.nix
@@ -1,13 +1,9 @@
 {
   wayland.windowManager.hyprland.settings.windowrule = [
-    "float, title:^(.*Bitwarden Password Manager.*)$"
+    # Floating Windows
+    "float, title:^(Friends List)$, class:^(steam)$"
 
-    "float, title:^(Picture-in-Picture)$"
-    "pin, title:^(Picture-in-Picture)$"
-
-    "workspace special silent, title:^(Zen — Sharing Indicator)$"
-    "workspace special silent, title:^(.*is sharing (your screen|a window)\.)$"
-
+    # Games
     # "content game, class:^(steam_app_.*)$"
     # "content game, title:.*\.exe"
     # "workspace 2, content: game"
@@ -15,7 +11,7 @@
     # "immediate, content: game"
     # "fullscreen, content: game"
 
-    # Temp while the above doesn't work
+    ## Temp while the above doesn't work
     "workspace 2, class:^(steam_app_.*)$"
     "idleinhibit always, class:^(steam_app_.*)$"
     "immediate, class:^(steam_app_.*)$"
@@ -24,7 +20,29 @@
     "workspace 2, class:^(.*\.exe)$"
     "idleinhibit class:^(.*\.exe)$"
     "fullscreen, class:^(.*\.exe)$"
+    "immediate, class:^(.*\.exe)$"
 
+    # Custom Windows
+    "float, class:ghostty.small"
+    "size 35% 35%, class:ghostty.small"
+    "move onscreen cursor -50% -50%, class:ghostty.small"
+
+    "float, class:ghostty.tui"
+    "move onscreen 100%-w-50 50, class:ghostty.tui"
+    "stayfocused, class:ghostty.tui"
+    "dimaround, class:ghostty.tui"
+
+    # Workspace Assignments
+    "workspace 8 silent, title:^(Proton Mail)$"
+    "workspace 9 silent, class:^(vesktop)$"
+    "workspace 10 silent, class:^(spotify)$"
+
+    # PiP
+    "float, title:^(Picture-in-Picture)$"
+    "pin, title:^(Picture-in-Picture)$"
+    "keepaspectratio, title:^(Picture-in-Picture)$"
+
+    # Dialogue Windows
     "center, title:^(Open File)(.*)$"
     "center, title:^(Select a File)(.*)$"
     "center, title:^(Choose wallpaper)(.*)$"
@@ -39,33 +57,18 @@
     "float, title:^(Library)(.*)$"
     "float, title:^(File Upload)(.*)$"
 
-    "float, class:ghostty.small"
-    "size 35% 35%, class:ghostty.small"
-    "move onscreen cursor -50% -50%, class:ghostty.small"
-
-    "float, class:ghostty.tui"
-    "move onscreen 100%-w-50 50, class:ghostty.tui"
-    "stayfocused, class:ghostty.tui"
-    "dimaround, class:ghostty.tui"
-
-    "workspace 8 silent, title:^(Proton Mail)$"
-    "workspace 9 silent, class:^(vesktop)$"
-    "workspace 10 silent, class:^(spotify)$"
-
+    ## General Window Rules
+    "noshadow, floating: 0"
     "idleinhibit focus, class:^(mpv)$"
     "idleinhibit focus, class:^(zen)$, title:^(.*YouTube.*)$"
     "idleinhibit fullscreen, class:^(zen)$"
 
-    "dimaround, class:^(gcr-prompter)$"
-    "dimaround, class:^(xdg-desktop-portal-gtk)$"
-    "dimaround, class:^(zen)$, title:^(File Upload)$"
-
+    # xwayland
+    "noblur, class:^()$,title:^()$"
+    ## "noblur, xwayland:1"
+    "nodim, xwayland:1"
+    "noshadow, xwayland:1"
     "rounding 0, xwayland:1"
-    "noblur, xwayland:1"
-    "center, class:^(.*jetbrains.*)$, title:^(Confirm Exit|Open Project|win424|win201|splash)$"
-    "size 640 400, class:^(.*jetbrains.*)$, title:^(splash)$"
-
-    "scrolltouchpad 0.1, class:^(zen|firefox)$"
-    "scrolltouchpad 0.1, class:^(xdg-desktop-portal-gtk)$"
+    "nofocus, class:^$, title:^$, xwayland:1, floating:1, fullscreen:0, pinned:0"
   ];
 }
