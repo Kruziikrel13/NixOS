a546dad85448b6dabb7235486f6331542b0d2284
kruziikrel13 <dev@michaelpetersen.io>
2025-07-11 22:54:00 +1000

    fix: plymouth center theme on startup

--- system/core/boot.nix
+++ system/core/boot.nix
@@ -12,7 +12,16 @@
     plymouth = {
       enable = true;
       theme = "hexagon_dots_alt";
-      themePackages = [pkgs.adi1090x-plymouth-themes];
+      themePackages = [
+        (pkgs.adi1090x-plymouth-themes.overrideAttrs (old: {
+          installPhase =
+            old.installPhase
+            + ''
+              find $out/share/plymouth/themes/ -name \*.script -exec sed -i 's/Window.GetX()/Window.GetX(0)/g' {} \;
+              find $out/share/plymouth/themes/ -name \*.script -exec sed -i 's/Window.GetY()/Window.GetY(0)/g' {} \;
+            '';
+        }))
+      ];
     };
     tmp.cleanOnBoot = true;
     kernelParams = ["systemd.show_status=auto" "amd_pstate=active"] ++ lib.optionals config.boot.plymouth.enable ["plymouth.use-simpledrm"];
