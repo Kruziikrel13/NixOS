e2700b18b710317bb79126ff907683b18b787d78
kruziikrel13 <dev@michaelpetersen.io>
2025-07-16 10:45:41 +1000

    system(hyprland): define hyprland substituters in hyprland file

--- system/programs/hyprland/default.nix
+++ system/programs/hyprland/default.nix
@@ -20,5 +20,8 @@
     withUWSM = true;
     package = inputs.hyprland.packages.${pkgs.system}.hyprland;
     portalPackage = inputs.hyprland.packages.${pkgs.system}.xdg-desktop-portal-hyprland;
+  nix.settings = {
+    substituters = ["https://hyprland.cachix.org"];
+    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
   };
 }
