8876352b4453129a59aed8208179a9658ecab268
kruziikrel13 <dev@michaelpetersen.io>
2025-07-21 21:48:29 +1000

    hyprland: added hyprland nixos module

--- modules/default.nix
+++ modules/default.nix
@@ -1,5 +1,9 @@
 self: inputs: {
+  nixosModules = {
+    hyprland = import ./hyprland self inputs.hyprland inputs.home-manager;
+  };
   homeManagerModules = {
     quickshell = import ./quickshell.nix self inputs.quickshell inputs.home-manager;
+    hyprland = import ./hyprland/home.nix self inputs.hyprland;
   };
 }
