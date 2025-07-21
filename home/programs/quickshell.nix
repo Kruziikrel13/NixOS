b70c595bc519e5a9fcc60e195be109ab5c59baea
kruziikrel13 <dev@michaelpetersen.io>
2025-07-21 22:25:07 +1000

    only enable quickshell if hyprland is enabled

--- home/programs/quickshell.nix
+++ home/programs/quickshell.nix
@@ -1,20 +1,25 @@
 {
   self,
   pkgs,
+  osConfig,
+  lib,
   ...
-}: {
-  imports = [self.homeManagerModules.quickshell];
+}: let
+  inherit (lib.modules) mkIf;
+in
+  mkIf osConfig.programs.hyprland.enable {
+    imports = [self.homeManagerModules.quickshell];
 
-  programs.quickshell = {
-    enable = true;
-    systemd.enable = true;
-    extraPackages = with pkgs.qt6; [qtimageformats qt5compat qtmultimedia];
-    config = /etc/nixos/.config/quickshell;
-  };
+    programs.quickshell = {
+      enable = true;
+      systemd.enable = true;
+      extraPackages = with pkgs.qt6; [qtimageformats qt5compat qtmultimedia];
+      config = /etc/nixos/.config/quickshell;
+    };
 
-  qt = {
-    enable = true;
-    platformTheme.name = "qtct";
-    style.name = "adwaita";
-  };
-}
+    qt = {
+      enable = true;
+      platformTheme.name = "qtct";
+      style.name = "adwaita";
+    };
+  }
