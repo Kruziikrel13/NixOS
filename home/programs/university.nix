4b05e40b27febe8cf7aa5f20933bb39290fd2068
kruziikrel13 <dev@michaelpetersen.io>
2025-08-08 16:13:45 +1000

    fix: use master version of unity hub with fix for projects

--- home/programs/university.nix
+++ home/programs/university.nix
@@ -1,4 +1,18 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  inputs,
+  ...
+}: let
+  pkgs-master = import inputs.nixpkgs-master {
+    inherit (pkgs) system;
+    config = {
+      allowUnfree = true;
+      permittedInsecurePackages = [
+        "libxml2-2.13.8"
+      ];
+    };
+  };
+in {
   programs.obs-studio.enable = true;
-  home.packages = [pkgs.slack pkgs.unityhub];
+  home.packages = [pkgs.slack pkgs-master.unityhub];
 }
