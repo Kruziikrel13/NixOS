35b90b6b36a6b4dd84f1d50e88349aff0e715387
kruziikrel13 <dev@michaelpetersen.io>
2025-08-11 10:24:41 +1000

    sherlock: add more exclusions to Sherlock list and enable various features

--- home/programs/sherlock.nix
+++ home/programs/sherlock.nix
@@ -3,17 +3,33 @@
   imports = [inputs.sherlock.homeManagerModules.default];
   programs.sherlock = {
     enable = true;
+    runAsService = true;
     settings = {
       aliases = {
         vesktop = {
           name = "Discord";
         };
+        zen = {
+          name = "firefox";
+        };
       };
       ignore = ''
         Qt*
         uuctl
+        Protontricks
+        Neovim*
+        Ghostty
+        bottom
+        Winetricks
+        Solaar
       '';
-      config.debug.try_suppress_warnings = true;
+      config = {
+        debug.try_suppress_warnings = true;
+        backdrop.enable = true;
+        caching.enable = true;
+        behavior.global_prefix = "uwsm app --";
+      };
+
       launchers = [
         {
           name = "Weather";
