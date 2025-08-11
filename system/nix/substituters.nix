dc1651466b8ace370689cb12b0be20931a9227d4
kruziikrel13 <dev@michaelpetersen.io>
2025-08-11 10:23:42 +1000

    refactor: remove unnecessary yazi cache entries

--- system/nix/substituters.nix
+++ system/nix/substituters.nix
@@ -5,7 +5,6 @@
 
       "https://nix-community.cachix.org"
       "https://nix-gaming.cachix.org"
-      "https://yazi.cachix.org"
     ];
 
     trusted-public-keys = [
@@ -13,7 +12,6 @@
 
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
-      "yazi.cachix.org-1:Dcdz63NZKfvUCbDGngQDAZq6kOroIrFoyO064uvLh8k="
     ];
   };
 }
