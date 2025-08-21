36a78ce253ba3523ab9b45cb5a750c1f220cbf97
kruziikrel13 <dev@michaelpetersen.io>
2025-08-21 19:53:05 +1000

    refactor: use sherlock from home manager

--- home/programs/sherlock.nix
+++ home/programs/sherlock.nix
@@ -1,45 +1,43 @@
-{inputs, ...}: {
-  disabledModules = ["${inputs.home-manager}/modules/programs/sherlock.nix"];
-  imports = [inputs.sherlock.homeManagerModules.default];
+{
+  inputs,
+  pkgs,
+  ...
+}: {
   programs.sherlock = {
     enable = true;
-    runAsService = true;
+    systemd.enable = true;
+    package = inputs.sherlock.packages.${pkgs.system}.default;
     settings = {
-      aliases = {
-        vesktop = {
-          name = "Discord";
-        };
-        zen = {
-          name = "firefox";
-        };
-      };
-      ignore = ''
-        Qt*
-        uuctl
-        Protontricks
-        Neovim*
-        Ghostty
-        bottom
-        Winetricks
-        Solaar
-      '';
       config = {
         debug.try_suppress_warnings = true;
         backdrop.enable = true;
         caching.enable = true;
         behavior.global_prefix = "uwsm app --";
       };
-
-      launchers = [
-        {
-          name = "App Launcher";
-          type = "app_launcher";
-          args = {};
-          priority = 2;
-          home = "Home";
-        }
-      ];
     };
+    aliases = {
+      vesktop = {name = "Discord";};
+      zen = {name = "firefox";};
+    };
+    ignore = ''
+      Qt*
+      uuctl
+      Protontricks
+      Neovim*
+      Ghostty
+      bottom
+      Winetricks
+      Solaar
+    '';
+    launchers = [
+      {
+        name = "App Launcher";
+        type = "app_launcher";
+        args = {};
+        priority = 2;
+        home = "Home";
+      }
+    ];
   };
   wayland.windowManager.hyprland.settings."$dmenu" = "sherlock";
 }
