6075594bd50f07db63dc12862e066b3417ba728d
kruziikrel13 <dev@michaelpetersen.io>
2025-09-02 00:15:20 +1000

    feat: installed hyprqt6engine packed to environment

--- system/programs/default.nix
+++ system/programs/default.nix
@@ -1,5 +1,15 @@
-{paths, ...}: {
+{
+  paths,
+  self,
+  pkgs,
+  ...
+}: {
   imports = paths.scanPaths ./.;
+  environment.systemPackages = [self.packages.${pkgs.system}.hyprqt6engine pkgs.kdePackages.breeze-icons pkgs.kdePackages.breeze];
+  qt.enable = true;
+  environment.sessionVariables = {
+    QT_QPA_PLATFORMTHEME = "hyprqt6engine";
+  };
 
   programs = {
     hyprland = {
