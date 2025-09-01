e7b81da0902990c657f5e3db91bc529501129b43
kruziikrel13 <dev@michaelpetersen.io>
2025-09-02 00:15:08 +1000

    feat: added hyprqt6engine package

--- packages/default.nix
+++ packages/default.nix
@@ -1,1 +1,4 @@
-pkgs: {bibata-hyprcursor = pkgs.callPackage ./bibata-hyprcursor {};}
+pkgs: {
+  bibata-hyprcursor = pkgs.callPackage ./bibata-hyprcursor {};
+  hyprqt6engine = pkgs.qt6Packages.callPackage ./hyprqt6engine {};
+}
