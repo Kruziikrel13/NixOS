8fe4ee71d9bba7874135cdccbc81ab9bc4ca225e
kruziikrel13 <dev@michaelpetersen.io>
2025-09-07 21:51:40 +1000

    feat: re-add unityhub and support it with rider

--- home/programs/rider.nix
+++ home/programs/rider.nix
@@ -21,15 +21,37 @@ let
   extra-lib = [ ];
   rider = pkgs.jetbrains.rider.overrideAttrs (attrs: {
     postInstall = (attrs.postInstall or "") + ''
-       # Wrap rider with extra tools and libraries
+      # Wrap rider with extra tools and libraries
       mv $out/bin/rider $out/bin/.rider-toolless
       makeWrapper $out/bin/.rider-toolless $out/bin/rider \
         --argv0 rider \
         --prefix PATH : "${lib.makeBinPath extra-pkgs}" \
         --prefix LD_LIBRARY_PATH : "${lib.makeLibraryPath extra-lib}"
+
+      # Unity Rider Fix
+      shopt -s extglob
+      ln -s $out/rider/!(bin) $out/
+      shopt -u extglob
     '';
   });
 in
 {
-  home.packages = [ rider ];
+  home.packages = [
+    rider
+    pkgs.unityhub
+  ];
+
+  home.file.rider = {
+    target = ".local/share/applications/jetbrains-rider.desktop";
+    source = "${
+      pkgs.makeDesktopItem {
+        name = "jetbrains-rider";
+        desktopName = "Rider";
+        exec = "\"${rider}/bin/rider\"";
+        icon = "rider";
+        type = "Application";
+        extraConfig.NoDisplay = "true";
+      }
+    }/share/applications/jetbrains-rider.desktop";
+  };
 }
