a6d85702badafc605b293f16e3d45e01f5e35a46
Kruziikrel13 <dev@michaelpetersen.io>
2025-09-11 13:44:03 +1000

    aridhol: hyprland laptop optimizations and gestures

--- hosts/aridhol/home/extra.nix
+++ hosts/aridhol/home/extra.nix
@@ -1,4 +1,16 @@
 { pkgs, ... }:
 {
   home.packages = [ pkgs.sutils ];
+  wayland.windowManager.hyprland.settings = {
+    animations.enabled = false;
+    decoration = {
+      shadow.enabled = false;
+      blur.enabled = false;
+    };
+    gesture = [
+      "3, left, workspace, m+1 "
+      "3, right, workspace, m-1 "
+      "3, up, float"
+    ];
+  };
 }
