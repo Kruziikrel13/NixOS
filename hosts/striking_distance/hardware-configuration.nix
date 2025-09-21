922c785c5e0069db064f3b5be6beeae7810691fc
kruziikrel13 <dev@michaelpetersen.io>
2025-09-21 20:06:41 +1000

    striking-distance: update config for new pc hardware

--- hosts/striking_distance/hardware-configuration.nix
+++ hosts/striking_distance/hardware-configuration.nix
@@ -8,24 +8,24 @@
   modulesPath,
   ...
 }:
-{
-  imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
 
-  boot = {
-    initrd = {
-      availableKernelModules = [
-        "nvme"
-        "xhci_pci"
-        "ahci"
-        "usb_storage"
-        "usbhid"
-        "sd_mod"
-      ];
-      kernelModules = [ ];
-    };
-    kernelModules = [ "kvm-amd" ];
-    extraModulePackages = [ ];
-  };
+{
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+  ];
+
+  boot.initrd.availableKernelModules = [
+    "nvme"
+    "xhci_pci"
+    "ahci"
+    "thunderbolt"
+    "usbhid"
+    "usb_storage"
+    "sd_mod"
+  ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-amd" ];
+  boot.extraModulePackages = [ ];
 
   fileSystems = {
     "/" = {
@@ -36,28 +36,14 @@
       device = "/dev/disk/by-uuid/543D-56A2";
       fsType = "vfat";
       options = [
-        "fmask=0077"
-        "dmask=0077"
+        "fmask=0022"
+        "dmask=0022"
       ];
     };
   };
 
   swapDevices = [ ];
-
-  networking = {
-    useDHCP = lib.mkDefault true;
-    # interfaces.enp5s0.useDHCP = lib.mkDefault true;
-  };
-
-  nixpkgs = {
-    hostPlatform = lib.mkDefault "x86_64-linux";
-  };
-
-  hardware = {
-    cpu = {
-      amd = {
-        updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-      };
-    };
-  };
+  networking.useDHCP = lib.mkDefault true;
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
