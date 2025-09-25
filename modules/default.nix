5d8897736fb845aa1bdff45c59aabbd297ded868
kruziikrel13 <dev@michaelpetersen.io>
2025-09-25 23:35:08 +1000

    nixosModules: added a nixosModule for the antec flux pro display case module

--- modules/default.nix
+++ modules/default.nix
@@ -2,6 +2,7 @@ self: inputs: {
   nixosModules = {
     hyprland = import ./nixosModules/hyprland.nix self inputs.hyprland;
     gnome = import ./nixosModules/gnome.nix self;
+    antec-flux-pro = import ./nixosModules/antec.nix self;
   };
   homeManagerModules = {
     quickshell = import ./homeManagerModules/quickshell.nix self inputs.quickshell inputs.home-manager;
---
+++ modules/nixosModules/antec.nix
@@ -0,0 +1,92 @@
+self:
+{
+  config,
+  pkgs,
+  lib,
+  self,
+  ...
+}:
+let
+  inherit (lib.modules) mkIf;
+  inherit (lib.meta) getExe;
+  inherit (lib.types)
+    str
+    ;
+  inherit (lib.options)
+    mkEnableOption
+    mkOption
+    mkPackageOption
+    ;
+
+  cfg = config.services.antec-flux-pro;
+in
+{
+  options.services.antec-flux-pro = {
+    enable = mkEnableOption "Antec Flux Pro Display Service";
+    package = mkPackageOption self.packages.${pkgs.system} "antec-flux-pro-display" {
+      nullable = false;
+    };
+    cpu-device = mkOption {
+      type = str;
+      default = "k10temp";
+      description = "CPU temperature device name";
+    };
+    cpu-temp-type = mkOption {
+      type = str;
+      default = "tctl";
+      description = "CPU temperature sensor label";
+    };
+    gpu-device = mkOption {
+      type = str;
+      default = "amdgpu";
+      description = "GPU temperature device name";
+    };
+    gpu-temp-type = mkOption {
+      type = str;
+      default = "edge";
+      description = "GPU temperature sensor label";
+    };
+  };
+  config = mkIf cfg.enable {
+    environment = {
+      systemPackages = [
+        (cfg.package.override {
+          cpu_device = cfg.cpu-device;
+          cpu_temp_type = cfg.cpu-temp-type;
+          gpu_device = cfg.gpu-device;
+          gpu_temp_type = cfg.gpu-temp-type;
+        })
+      ];
+      etc."antec-flux-pro-display/config.conf".text = ''
+        cpu_device=${cfg.cpu-device}
+        cpu_temp_type=${cfg.cpu-temp-type}
+        gpu_device=${cfg.gpu-device}
+        gpu_temp_type=${cfg.gpu-temp-type}
+        update_interval=1000
+      '';
+    };
+    services.udev.extraRules = ''
+      SUBSYSTEM=="usb", ATTR{idVendor}=="2022", ATTR{idProduct}=="0522", MODE="0666", GROUP="plugdev"
+      SUBSYSTEM=="usb", ATTR{idVendor}=="2022", ATTR{idProduct}=="0522", MODE="0666", GROUP="plugdev", TAG+="uaccess"
+    '';
+    systemd.services.antec-flux-pro-display = {
+      unitConfig = {
+        Description = "Antec Flux Pro Display Service";
+        StartLimitIntervalSec = "0";
+      };
+
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = getExe cfg.package;
+        Restart = "always";
+        RestartSec = "5";
+        ProtectSystem = "strict";
+        ProtectHome = "true";
+        PrivateTmp = "true";
+        NoNewPrivileges = "true";
+      };
+
+      wantedBy = [ "multi-user.target" ];
+    };
+  };
+}
