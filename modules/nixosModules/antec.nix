9641bda32fe2ff3ff9ce0b73e6c86109e8262cc1
kruziikrel13 <dev@michaelpetersen.io>
2025-10-08 01:15:47 +1000

    nixosModules(antec): refactored antec option
    - now at hardware.antec
    - removed config options that have been merged into the antec package

--- modules/nixosModules/antec.nix
+++ modules/nixosModules/antec.nix
@@ -14,72 +14,61 @@ let
   inherit (lib.options)
     mkEnableOption
     mkOption
-    mkPackageOption
     ;
-
-  cfg = config.services.antec-flux-pro;
+  cfg = config.hardware.antec;
+  myPkgs = self.packages.${pkgs.system};
 in
 {
-  options.services.antec-flux-pro = {
+  options.hardware.antec = {
     enable = mkEnableOption "Antec Flux Pro Display Service";
-    package = mkPackageOption self.packages.${pkgs.system} "antec-flux-pro-display" {
-      nullable = false;
-    };
     cpu-device = mkOption {
       type = str;
-      default = "k10temp";
       description = "CPU temperature device name";
     };
     cpu-temp-type = mkOption {
       type = str;
-      default = "tctl";
       description = "CPU temperature sensor label";
     };
     gpu-device = mkOption {
       type = str;
-      default = "amdgpu";
       description = "GPU temperature device name";
     };
     gpu-temp-type = mkOption {
       type = str;
-      default = "edge";
       description = "GPU temperature sensor label";
     };
   };
-  config = mkIf cfg.enable {
-    environment = {
-      systemPackages = [ cfg.package ];
-      etc."antec-flux-pro-display/config.conf".text = ''
-        cpu_device=${cfg.cpu-device}
-        cpu_temp_type=${cfg.cpu-temp-type}
-        gpu_device=${cfg.gpu-device}
-        gpu_temp_type=${cfg.gpu-temp-type}
-        update_interval=1000
-      '';
-    };
-    services.udev.extraRules = ''
-      SUBSYSTEM=="usb", ATTR{idVendor}=="2022", ATTR{idProduct}=="0522", MODE="0666", GROUP="plugdev"
-      SUBSYSTEM=="usb", ATTR{idVendor}=="2022", ATTR{idProduct}=="0522", MODE="0666", GROUP="plugdev", TAG+="uaccess"
-    '';
-    systemd.services.antec-flux-pro-display = {
-      unitConfig = {
-        Description = "Antec Flux Pro Display Service";
-        StartLimitIntervalSec = "5";
+  config =
+    let
+      package = myPkgs.antec-flux-pro-display.override {
+        cpu_device = cfg.cpu-device;
+        cpu_temp_type = cfg.cpu-temp-type;
+        gpu_device = cfg.gpu-device;
+        gpu_temp_type = cfg.gpu-temp-type;
       };
+    in
+    mkIf cfg.enable {
+      environment.systemPackages = [ package ];
+      services.udev.packages = [ package.udev ];
+      systemd.services.antec-flux-pro-display = {
+        unitConfig = {
+          Description = "Antec Flux Pro Display Service";
+          StartLimitIntervalSec = "5";
+        };
 
-      serviceConfig = {
-        Type = "simple";
-        ExecStartPre = "${pkgs.coreutils}/bin/sleep 5";
-        ExecStart = getExe cfg.package;
-        Restart = "always";
-        RestartSec = "5";
-        ProtectSystem = "strict";
-        ProtectHome = "true";
-        PrivateTmp = "true";
-        NoNewPrivileges = "true";
+        serviceConfig = {
+          Type = "simple";
+          ExecStartPre = "${pkgs.coreutils}/bin/sleep 5";
+          ExecStart = getExe package;
+          Restart = "always";
+          RestartSec = "5";
+          ProtectSystem = "strict";
+          ProtectHome = "true";
+          PrivateTmp = "true";
+          NoNewPrivileges = "true";
+        };
+
+        wantedBy = [ "multi-user.target" ];
       };
-
-      wantedBy = [ "multi-user.target" ];
     };
-  };
 }
