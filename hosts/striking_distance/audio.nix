a26acefacbf81952b9ff5e13c0f7568bb1cac107
kruziikrel13 <dev@michaelpetersen.io>
2025-10-09 00:53:21 +1000

    striking-distance; simplify audio config and use nix-gaming module for latency

--- hosts/striking_distance/audio.nix
+++ hosts/striking_distance/audio.nix
@@ -1,46 +1,13 @@
-{ lib, ... }:
+{ inputs, ... }:
 {
-  services = {
-    pipewire = {
+  imports = [ inputs.nix-gaming.nixosModules.pipewireLowLatency ];
+  services.pipewire = {
+    enable = true;
+    lowLatency = {
       enable = true;
-      audio.enable = true;
-      wireplumber.enable = true;
-      alsa.enable = true;
-      alsa.support32Bit = true;
-      jack.enable = true;
-      pulse.enable = true;
-      extraConfig.pipewire = {
-        "10-clock-rate" = {
-          "context.properties" = {
-            "default.clock.rate" = 384000;
-            "default.clock.allowed-rates" = [
-              44100
-              48000
-              96000
-              176400
-              192000
-              352800
-              384000
-            ];
-            "default.clock.quantum" = 1024;
-            "default.clock.min-quantum" = 32;
-            "default.clock.max-quantum" = 2048;
-          };
-        };
-      };
-      extraConfig.client = {
-        "11-resample-max" = {
-          "stream.properties" = {
-            "resample.quality" = 10;
-          };
-        };
-      };
+      quantum = 32;
+      rate = 384000;
     };
-    udev.extraRules = ''
-      KERNEL=="rtc0", GROUP="audio"
-      KERNEL=="hpet", GROUP="audio"
-    '';
-    pulseaudio.enable = lib.mkForce false;
   };
 
   security.pam.loginLimits = [
