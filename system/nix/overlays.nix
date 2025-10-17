2d5f2709a4cf41b416cdd56f0e52ffb57a55b03c
kruziikrel13 <dev@michaelpetersen.io>
2025-10-17 13:56:46 +1000

    fix: remove .gitattributes from grayjay v10 to build correctly

--- system/nix/overlays.nix
+++ system/nix/overlays.nix
@@ -1,3 +1,10 @@
 {
-  nixpkgs.overlays = [ ];
+  nixpkgs.overlays = [
+    (final: prev: {
+      # Temporary override until v10 is added to nixpkgs
+      grayjay = prev.grayjay.overrideAttrs {
+        postUnpack = "rm -f $sourceRoot/prebuilt/.gitattributes";
+      };
+    })
+  ];
 }
