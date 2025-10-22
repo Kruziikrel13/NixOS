230331f5aa4e710309a84fa8763ff5fac35d3b46
kruziikrel13 <dev@michaelpetersen.io>
2025-10-22 13:56:25 +1000

    feat: make dotnet 3 available globally through overlay

--- system/nix/overlays.nix
+++ system/nix/overlays.nix
@@ -1,3 +1,15 @@
+{ inputs, pkgs, ... }:
+let
+
+  dotnet3-available = import inputs.nixpkgs-2022 {
+    inherit (pkgs) system;
+    config.allowUnfree = true;
+  };
+in
 {
-  nixpkgs.overlays = [ ];
+  nixpkgs.overlays = [
+    (final: prev: {
+      dotnet-sdk_3 = dotnet3-available.dotnet-sdk_3;
+    })
+  ];
 }
