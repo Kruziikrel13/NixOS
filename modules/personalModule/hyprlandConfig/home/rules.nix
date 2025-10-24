82601e9fb6f2e7e73889db82a83ae2430060bab3
kruziikrel13 <dev@michaelpetersen.io>
2025-10-24 22:15:35 +1000

    hyprlandConfig: update rules to be on one line following new hyprland update

--- modules/personalModule/hyprlandConfig/home/rules.nix
+++ modules/personalModule/hyprlandConfig/home/rules.nix
@@ -13,14 +13,8 @@
     "idleinhibit fullscreen, tag:video"
 
     # Custom Windows
-    "float, class:ghostty.small"
-    "size 35% 35%, class:ghostty.small"
-    "move onscreen cursor -50% -50%, class:ghostty.small"
-
-    "float, class:ghostty.tui"
-    "move onscreen 100%-w-50 50, class:ghostty.tui"
-    "stayfocused, class:ghostty.tui"
-    "dimaround, class:ghostty.tui"
+    "float, size 35% 35%, move onscreen cursor -50% -50%, class:ghostty.small"
+    "float, stayfocused, dimaround, move onscreen 100%-w-50 50, class:ghostty.tui"
 
     # Workspace Assignments
     "workspace name:email, tag:email"
@@ -28,24 +22,15 @@
     "workspace name:music, tag:music"
 
     # PiP
-    "float, title:^(Picture-in-Picture)$"
-    "pin, title:^(Picture-in-Picture)$"
-    "keepaspectratio, title:^(Picture-in-Picture)$"
+    "float, pin, keepaspectratio, title:^(Picture-in-Picture)$"
 
     # Dialogue Windows
-    "center, title:^(Open File)(.*)$"
-    "center, title:^(Select a File)(.*)$"
-    "center, title:^(Choose wallpaper)(.*)$"
-    "center, title:^(Save As)(.*)$"
-    "center, title:^(Library)(.*)$"
-    "center, title:^(File Upload)(.*)$"
-    "float, title:^(Open File)(.*)$"
-    "float, title:^(Select a File)(.*)$"
-    "float, title:^(Choose wallpaper)(.*)$"
-    "float, title:^(Open Folder)(.*)$"
-    "float, title:^(Save As)(.*)$"
-    "float, title:^(Library)(.*)$"
-    "float, title:^(File Upload)(.*)$"
+    "center, float, title:^(Open File)(.*)$"
+    "center, float, title:^(Select a File)(.*)$"
+    "center, float, title:^(Choose wallpaper)(.*)$"
+    "center, float, title:^(Save As)(.*)$"
+    "center, float, title:^(Library)(.*)$"
+    "center, float, title:^(File Upload)(.*)$"
 
     ## General Window Rules
     "noshadow, floating: 0"
@@ -54,9 +39,7 @@
 
     # xwayland
     "noblur, class:^()$,title:^()$"
-    "nodim, xwayland:1"
-    "noshadow, xwayland:1"
-    "rounding 0, xwayland:1"
+    "nodim, noshadow, rounding 0, xwayland:1"
     "nofocus, class:^$, title:^$, xwayland:1, floating:1, fullscreen:0, pinned:0"
     "noinitialfocus, class:^(jetbrains-).*,floating:1,title:^$|^\s$|^win\d+$"
     "noinitialfocus, class:^(Unity)$,floating:1"
