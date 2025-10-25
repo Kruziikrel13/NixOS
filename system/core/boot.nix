f6b97ebd5ee152c3895d1463ff17b6b9f00f987c
kruziikrel13 <dev@michaelpetersen.io>
2025-10-25 13:24:33 +1000

    boot: simplify boot configuration
    - remove redundant params
    - speed up boot times

--- system/core/boot.nix
+++ system/core/boot.nix
@@ -1,46 +1,27 @@
 {
-  pkgs,
   lib,
-  config,
   inputs,
   ...
 }:
 {
   imports = [ inputs.lanzaboote.nixosModules.lanzaboote ];
   boot = {
-    lanzaboote = {
-      enable = true;
-      pkiBundle = "/var/lib/sbctl";
-    };
-    plymouth = {
-      enable = false;
-      theme = "hexagon_dots_alt";
-      themePackages = [
-        (pkgs.adi1090x-plymouth-themes.overrideAttrs (old: {
-          # Fixes off centering of themes on multiple monitors
-          installPhase = old.installPhase + ''
-            find $out/share/plymouth/themes/ -name \*.script -exec sed -i 's/Window.GetX()/Window.GetX(0)/g' {} \;
-            find $out/share/plymouth/themes/ -name \*.script -exec sed -i 's/Window.GetY()/Window.GetY(0)/g' {} \;
-          '';
-        }))
-      ];
-    };
-    initrd.systemd.enable = true;
+    bootspec.enable = true;
     tmp.cleanOnBoot = true;
-    kernelParams = lib.optionals config.boot.plymouth.enable [ "plymouth.use-simpledrm" ];
     loader = {
       timeout = 5;
-      efi = {
-        canTouchEfiVariables = true;
-        efiSysMountPoint = "/boot";
-      };
+      efi.canTouchEfiVariables = true;
       systemd-boot = {
+        # Force disable when using lanzaboote
         enable = lib.mkForce false;
         editor = false;
         consoleMode = "max";
         configurationLimit = 10;
       };
     };
+    lanzaboote = {
+      enable = true;
+      pkiBundle = "/var/lib/sbctl";
+    };
   };
-  # TODO Support CPU Power Here? Or Kernel Specific CPU Power Support in Hosts
 }
