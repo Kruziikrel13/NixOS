408b9d0798fc70542e34bfa1c90dfb1525da4f66
kruziikrel13 <dev@michaelpetersen.io>
2025-10-28 22:10:34 +1000

    users: remove user description

--- system/core/users.nix
+++ system/core/users.nix
@@ -2,7 +2,6 @@
 {
   users.users.${username} = {
     isNormalUser = true;
-    description = "Primary user account for system";
     hashedPassword = "$7$CU..../....kUbGCK9CWgYVn7po7zdyz0$CxvNCDMqGFzOZBAO0iAhytSnonc.LuMyvv1FodplVaB";
     extraGroups = [
       "gamemode"
