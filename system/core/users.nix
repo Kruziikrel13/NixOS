de472cbc2fad8ea34a0a8c86dc47da37b66ef441
kruziikrel13 <dev@michaelpetersen.io>
2025-10-28 22:11:19 +1000

    users: add primary user to users group

--- system/core/users.nix
+++ system/core/users.nix
@@ -11,6 +11,7 @@ in
   users.users.${username} = {
     isNormalUser = true;
     hashedPassword = "$7$CU..../....kUbGCK9CWgYVn7po7zdyz0$CxvNCDMqGFzOZBAO0iAhytSnonc.LuMyvv1FodplVaB";
+    group = "users";
     extraGroups = coreGroups ++ [
       "wheel"
       "networkmanager"
