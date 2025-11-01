2572a1b0e3a06f547c59174de0110a02e3e6c7e2
kruziikrel13 <dev@michaelpetersen.io>
2025-11-01 17:21:01 +1000

    fix: lanzaboote needs to use it's own inputs as it's now incompatible with upstream

--- flake.nix
+++ flake.nix
@@ -32,10 +32,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    lanzaboote = {
-      url = "github:nix-community/lanzaboote?ref=v0.4.2";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    lanzaboote.url = "github:nix-community/lanzaboote";
 
     nix-gaming = {
       url = "github:fufexan/nix-gaming";
