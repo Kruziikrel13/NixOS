aafb101802a59731c4c40e277e8a546ab3630e1b
kruziikrel13 <dev@michaelpetersen.io>
2025-11-03 08:15:53 +1000

    programs(gaming): drop mumble

--- hosts/striking_distance/home/gaming.nix
+++ hosts/striking_distance/home/gaming.nix
@@ -7,7 +7,6 @@
   home.sessionVariables.WINEPREFIX = "${config.xdg.dataHome}/wine";
   home.packages = with pkgs; [
     wineWow64Packages.waylandFull
-    mumble
     heroic
     winetricks
     umu-launcher
