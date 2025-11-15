eef280f4f221ee81eecc45ced370e5665ace485b
kruziikrel13 <dev@michaelpetersen.io>
2025-11-15 12:11:41 +1000

    programs: add prism launcher

--- system/programs/default.nix
+++ system/programs/default.nix
@@ -5,7 +5,10 @@
 }:
 {
   imports = pathLib.scanPaths ./.;
-  environment.systemPackages = [ pkgs.runapp ];
+  environment.systemPackages = [
+    pkgs.runapp
+    pkgs.prismlauncher
+  ];
   programs = {
     dconf.enable = true;
     neovim.enable = true;
