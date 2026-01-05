1179a4837ae61e8df83f786083689f5b1fadfb01
kruziikrel13 <dev@michaelpetersen.io>
2026-01-05 20:19:54 +1000

    Revert "programs: remove vesktop"
    This reverts commit 420603ac89a8892c8607311cba1b65e7aece893f.

---
+++ home/programs/vesktop.nix
@@ -0,0 +1,16 @@
+{
+  programs.vesktop = {
+    enable = true;
+    settings = {
+      arRPC = true;
+      disableMinSize = true;
+      minimizeToTray = true;
+      splashBackground = "rgb(32, 32, 36)";
+      splashColor = "rgb(239, 239, 240)";
+      splashTheming = true;
+
+      hardwareAcceleration = true;
+      discordBranch = "stable";
+    };
+  };
+}
