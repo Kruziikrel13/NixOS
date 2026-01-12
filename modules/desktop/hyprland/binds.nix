89337eaae84af3de8fbe414e4d2b195e2b82e5fd
kruziikrel13 <dev@michaelpetersen.io>
2026-01-12 20:23:02 +1000

    hyprland: binds formatting

--- modules/desktop/hyprland/binds.nix
+++ modules/desktop/hyprland/binds.nix
@@ -1,74 +1,79 @@
 { lib, pkgs, ... }:
+let
+  inherit (lib.meta) getExe;
+in
 {
-  programs.hyprland.settings = {
-    bindm = [
-      "$mod, mouse:272, movewindow"
-      "$mod, mouse:273, resizewindow"
-    ];
+  programs.hyprland = {
+    settings = {
+      bindm = [
+        "$mod, mouse:272, movewindow"
+        "$mod, mouse:273, resizewindow"
+      ];
 
-    bindp = [
-      "$mod, Q, killactive"
-      "$mod SHIFT, Q, forcekillactive"
-    ];
+      bindp = [
+        "$mod, Q, killactive"
+        "$mod SHIFT, Q, forcekillactive"
+      ];
 
-    bind = [
-      "$mod, RETURN, exec, ${lib.meta.getExe pkgs.runapp} -- $terminal" # Launch Terminal
-      "$mod SHIFT, RETURN, exec, runapp -- $terminal --class=ghostty.small" # Launch Terminal
-      "$mod, Escape, exec, uwsm stop"
-      "$mod, D, exec, $dmenu" # Launcher
+      bind = [
+        "$mod, RETURN, exec, ${getExe pkgs.runapp} -- $terminal" # Launch Terminal
+        "$mod SHIFT, RETURN, exec, ${getExe pkgs.runapp} -- $terminal --class=ghostty.small" # Launch Terminal
+        "$mod, Escape, exec, uwsm stop"
+        "$mod, D, exec, $dmenu" # Launcher
 
-      "$mod, F, fullscreen"
-      "$mod, R, togglesplit"
-      "$mod, T, togglefloating"
-      "$mod, C, centerwindow"
+        "$mod, F, fullscreen"
+        "$mod, R, togglesplit"
+        "$mod, T, togglefloating"
+        "$mod, C, centerwindow"
 
-      "$mod, L, movefocus, r"
-      "$mod, H, movefocus, l"
-      "$mod, K, movefocus, u"
-      "$mod, J, movefocus, d"
+        "$mod, L, movefocus, r"
+        "$mod, H, movefocus, l"
+        "$mod, K, movefocus, u"
+        "$mod, J, movefocus, d"
 
-      "$mod, mouse_up, focusmonitor, r"
-      "$mod, mouse_down, focusmonitor, l"
+        "$mod, mouse_up, focusmonitor, r"
+        "$mod, mouse_down, focusmonitor, l"
 
-      # "$mod SHIFT, grave, movetoworkspace, special"
-      # "$mod, grave, togglespecialworkspace, special"
+        # "$mod SHIFT, grave, movetoworkspace, special"
+        # "$mod, grave, togglespecialworkspace, special"
 
-      "$mod, bracketleft, workspace, m-1"
-      "$mod, bracketright, workspace, m+1"
+        "$mod, bracketleft, workspace, m-1"
+        "$mod, bracketright, workspace, m+1"
 
-      "$mod SHIFT, bracketleft, focusmonitor, l"
-      "$mod SHIFT, bracketright, focusmonitor, r"
+        "$mod SHIFT, bracketleft, focusmonitor, l"
+        "$mod SHIFT, bracketright, focusmonitor, r"
 
-      # Master Binds
-      "$mod SHIFT, M, layoutmsg, swapwithmaster"
-      "$mod SHIFT, H, layoutmsg, swapprev"
-      "$mod SHIFT, L, layoutmsg, swapnext"
-      "$mod, TAB, layoutmsg, cyclenext"
-      "$mod SHIFT, TAB, cyclenext"
+        # Master Binds
+        "$mod SHIFT, M, layoutmsg, swapwithmaster"
+        "$mod SHIFT, H, layoutmsg, swapprev"
+        "$mod SHIFT, L, layoutmsg, swapnext"
+        "$mod, TAB, layoutmsg, cyclenext"
+        "$mod SHIFT, TAB, cyclenext"
 
-      "CTRL SHIFT, M, pass, class:^(vesktop)$"
-      "CTRL SHIFT, D, pass, class:^(vesktop)$"
-    ];
+        "CTRL SHIFT, M, pass, class:^(vesktop)$"
+        "CTRL SHIFT, D, pass, class:^(vesktop)$"
+      ];
 
-    bindl = [
-      ", XF86AudioNext, exec, playerctl next"
-      ", XF86AudioPlay, exec, playerctl play-pause"
-      ", XF86AudioPrev, exec, playerctl previous"
+      bindl = [
+        ", XF86AudioNext, exec, playerctl next"
+        ", XF86AudioPlay, exec, playerctl play-pause"
+        ", XF86AudioPrev, exec, playerctl previous"
 
-      ", XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
-      ", XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
-    ];
+        ", XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+        ", XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
+      ];
 
-    binde = [
-      "$mod, right, resizeactive, 25 0"
-      "$mod, left, resizeactive, -25 0"
-      "$mod, up, resizeactive, 0 -25"
-      "$mod, down, resizeactive, 0 25"
-    ];
+      binde = [
+        "$mod, right, resizeactive, 25 0"
+        "$mod, left, resizeactive, -25 0"
+        "$mod, up, resizeactive, 0 -25"
+        "$mod, down, resizeactive, 0 25"
+      ];
 
-    bindle = [
-      ", XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%- --limit 1"
-      ", XF86AudioRaiseVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+ --limit 1"
-    ];
+      bindle = [
+        ", XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%- --limit 1"
+        ", XF86AudioRaiseVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+ --limit 1"
+      ];
+    };
   };
 }
