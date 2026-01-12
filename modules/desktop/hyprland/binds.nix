008ea51829e40858c0f0119b2e78185d56e00269
kruziikrel13 <dev@michaelpetersen.io>
2026-01-12 20:27:35 +1000

    hyprland: re-add workspace submapping in extraConfig

--- modules/desktop/hyprland/binds.nix
+++ modules/desktop/hyprland/binds.nix
@@ -75,5 +75,32 @@ in
         ", XF86AudioRaiseVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+ --limit 1"
       ];
     };
+    # Workspaces Submap
+    extraConfig =
+      let
+        workspaces = lib.concatStringsSep "\n" (
+          lib.imap0 (
+            i: _:
+            let
+              key = toString (lib.mod (i + 1) 10); # 1,2,3,4,5,6,7,8,9,0
+              ws = toString (i + 1); # workspaces 1-10
+            in
+            "bind =, ${key}, workspace, ${ws}"
+          ) (lib.range 1 10)
+        );
+      in
+      ''
+        workspace = name:gaming, monitor:desc:GIGA-BYTE TECHNOLOGY CO. LTD. AORUS FI32U 21440B000115
+        workspace = name:email, monitor:desc:ViewSonic Corporation VX2758-C-MH V9M184500179, on-created-empty: runapp -- proton-mail
+        workspace = name:chat, monitor:desc:ViewSonic Corporation VX2758-C-MH V9M184500179, on-created-empty: runapp -- vesktop
+        bind = $mod, W, submap, workspaces
+        submap = workspaces
+        ${workspaces}
+        bind=, e, workspace, name:email
+        bind=, c, workspace, name:chat
+        bind=, g, workspace, name:gaming
+        bind=, catchall, submap, reset
+        submap = reset
+      '';
   };
 }
