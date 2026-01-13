d1a4a0c35b976027f34ebbec0bdc87d2cb95a56b
kruziikrel13 <dev@michaelpetersen.io>
2026-01-13 11:33:39 +1000

    cli: migrate all to modules

---
+++ modules/shell/default.nix
@@ -0,0 +1,22 @@
+{
+  lib',
+  lib,
+  config,
+  ...
+}:
+let
+  inherit (lib) mkDefault;
+  inherit (lib'.options) mkBoolOpt;
+in
+{
+  options.modules.shell.defaultSuite = mkBoolOpt false;
+  config = lib.mkIf config.modules.shell.defaultSuite {
+    modules.shell = {
+      rust-utils.enable = mkDefault true;
+      direnv.enable = mkDefault true;
+      zoxide.enable = mkDefault true;
+      eza.enable = mkDefault true;
+      bat.enable = mkDefault true;
+    };
+  };
+}
