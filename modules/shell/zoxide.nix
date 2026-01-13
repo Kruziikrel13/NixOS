4a17f4b54a635a47583ac8037b59ca921e4625ad
kruziikrel13 <dev@michaelpetersen.io>
2026-01-13 11:02:44 +1000

    zoxide: add ccd alias

--- modules/shell/zoxide.nix
+++ modules/shell/zoxide.nix
@@ -7,10 +7,12 @@
 let
   inherit (lib'.options) mkBoolOpt;
   cfg = config.modules.shell.zoxide;
+  inherit (config.programs.zoxide) package;
 in
 {
   options.modules.shell.zoxide.enable = mkBoolOpt false;
   config = lib.mkIf cfg.enable {
     programs.zoxide.enable = true;
+    environment.shellAliases.ccd = "${lib.meta.getExe package}";
   };
 }
