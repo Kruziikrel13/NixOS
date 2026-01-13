0a625f9b88ffa27d4b02ad8e8181011004f744a9
kruziikrel13 <dev@michaelpetersen.io>
2026-01-13 22:47:47 +1000

    hardware/ssd: add ssd mount opts to default fileSystems

--- modules/profiles/hardware/ssd.nix
+++ modules/profiles/hardware/ssd.nix
@@ -7,4 +7,8 @@ in
 mkIf (elem "ssd" config.modules.profiles.hardware) {
   services.fstrim.enable = true;
   boot.initrd.availableKernelModules = [ "nvme" ];
+  fileSystems = {
+    "/".options = [ "ssd" ];
+    "/home".options = [ "ssd" ];
+  };
 }
