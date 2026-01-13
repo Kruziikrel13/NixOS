7671a51e51635bfee5da100626f9488e1694df50
kruziikrel13 <dev@michaelpetersen.io>
2026-01-13 11:36:16 +1000

    helix: syntax fixes

--- modules/editors/helix.nix
+++ modules/editors/helix.nix
@@ -11,18 +11,18 @@ let
 in
 {
   options.modules.editors.helix.enable = mkBoolOpt (config.modules.editors.default == "helix");
-  config = lib.mkIf cfg.enable lib.mkMerge [
-    {
-      user.packages = [ pkgs.evil-helix ];
-    }
-    (
-      lib.mkIf config.modules.editors.default == "helix" {
+  config = lib.mkIf cfg.enable (
+    lib.mkMerge [
+      {
+        user.packages = [ pkgs.evil-helix ];
+      }
+      (lib.mkIf (config.modules.editors.default == "helix") {
         environment.variables.EDITOR = lib.mkOverride 900 "hx";
         environment.shellAliases = {
           vim = "hx";
           v = "hx";
         };
-      }
-    )
-  ];
+      })
+    ]
+  );
 }
