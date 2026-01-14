d04a4cd076ed726bdb713d40e6206d48334c30ee
kruziikrel13 <dev@michaelpetersen.io>
2026-01-14 15:38:12 +1000

    nvim: fix wrapper args and environment additions

--- modules/editors/nvim.nix
+++ modules/editors/nvim.nix
@@ -15,47 +15,44 @@ in
   options.modules.editors.nvim = with lib.types; {
     enable = mkBoolOpt (config.modules.editors.default == "nvim");
     extraPackages = mkOpt (listOf package) [ ];
-    finalPackage = lib.mkOption {
-      type = package;
-      readOnly = true;
-    };
   };
   config = lib.mkIf cfg.enable {
     nixpkgs.overlays = [ neovim-nightly-overlay.overlays.default ];
     home.configFiles."nvim".source = "${toString self}/config/sentinel.nvim";
     environment = {
-      systemPackages = [ cfg.finalPackage ];
+      systemPackages = [
+        (pkgs.wrapNeovimUnstable pkgs.neovim-unwrapped {
+          viAlias = true;
+          vimAlias = true;
+          waylandSupport = true;
+          wrapRc = false;
+          wrapperArgs = [
+            "--suffix"
+            "PATH"
+            ":"
+            (lib.makeBinPath (
+              with pkgs;
+              [
+                gcc
+                ripgrep
+                fzf
+                lazygit
+                tree-sitter
+                github-copilot-cli
+              ]
+              ++ cfg.extraPackages
+            ))
+          ];
+        })
+      ];
       shellAliases = {
         vimdiff = "nvim -d";
-        vi = "nvim";
-        vim = "nvim";
       };
       variables = {
         EDITOR = lib.mkOverride 900 "nvim";
         VISUAL = lib.mkOverride 900 "nvim";
       };
       pathsToLink = [ "/share/nvim" ];
-    };
-    modules.editors.nvim.finalPackage = pkgs.wrapNeovimUnstable pkgs.neovim-unwrapped {
-      viAlias = true;
-      vimAlias = true;
-      waylandSupport = true;
-      wrapperArgs = with pkgs; [
-        "--suffix"
-        "PATH"
-        ":"
-        (lib.makeBinPath (
-          [
-            gcc
-            ripgrep
-            fzf
-            lazygit
-            tree-sitter
-            github-copilot-cli
-          ]
-          ++ cfg.extraPackages
-        ))
-      ];
     };
   };
 }
