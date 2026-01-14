c254bbbbb2d62e115c64da919056b0b25bd4b52a
kruziikrel13 <dev@michaelpetersen.io>
2026-01-15 08:36:04 +1000

    hyprland: check only one autoLogin is enabled

--- modules/desktop/hyprland/default.nix
+++ modules/desktop/hyprland/default.nix
@@ -9,7 +9,13 @@
 let
   cfg = config.modules.desktop.hyprland;
 
-  inherit (lib) mkMerge mkDefault;
+  inherit (lib)
+    mkMerge
+    mkDefault
+    filter
+    length
+    attrValues
+    ;
   inherit (lib.modules) mkIf;
   inherit (lib.options) mkEnableOption;
   inherit (lib'.options) mkOpt mkBoolOpt;
@@ -29,6 +35,18 @@ in
 
   config = mkIf cfg.enable (mkMerge [
     {
+      assertions = [
+        {
+          assertion =
+            !config.modules.desktop.hyprland.autoLogin
+            || (
+              length (
+                filter (desktop: desktop ? autoLogin && desktop.autoLogin) (attrValues config.modules.desktop)
+              ) <= 1
+            );
+          message = "Only one desktop module can have autoLogin enabled at a time. Hyprland has autoLogin enabled, but another desktop module also has it enabled.";
+        }
+      ];
       modules.services.quickshell.enable = mkDefault true;
       modules.services.hyprlauncher.enable = mkDefault true;
       user.packages = with pkgs; [
