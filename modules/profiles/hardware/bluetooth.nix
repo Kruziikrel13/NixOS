5eb97481a8cd22830d9c3ae0bde0485f52d0b4df
kruziikrel13 <dev@michaelpetersen.io>
2026-01-14 18:15:50 +1000

    modules: impl bluetooth

---
+++ modules/profiles/hardware/bluetooth.nix
@@ -0,0 +1,29 @@
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
+let
+  inherit (lib) elem;
+  inherit (lib.modules) mkIf;
+in
+{
+
+  config = mkIf (elem "bluetooth" config.modules.profiles.hardware) {
+    hardware.bluetooth = {
+      enable = true;
+      settings = {
+        General.ControllerMode = "bredr";
+        Policy.ReconnectAttempts = 0;
+      };
+    };
+
+    # Brute force a reset after waking up from sleep, as some bluetooth devices
+    # will fail to connect to a system that's been suspended at some point.
+    powerManagement.resumeCommands = ''
+      ${pkgs.util-linux}/bin/rfkill block bluetooth
+      ${pkgs.util-linux}/bin/rfkill unblock bluetooth
+    '';
+  };
+}
