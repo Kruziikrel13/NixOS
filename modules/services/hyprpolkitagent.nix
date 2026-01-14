d7da52fe3cdf40d900129f868e5cafd0bfd9cb78
kruziikrel13 <dev@michaelpetersen.io>
2026-01-14 20:27:30 +1000

    services: impl hyprpolkitagent

---
+++ modules/services/hyprpolkitagent.nix
@@ -0,0 +1,31 @@
+{
+  config,
+  lib',
+  lib,
+  pkgs,
+  ...
+}:
+let
+
+  inherit (lib.modules) mkIf;
+  inherit (lib.meta) getExe;
+  inherit (lib.options) mkEnableOption;
+  cfg = config.modules.services.hyprpolkitagent;
+in
+{
+  options.modules.services.hyprpolkitagent.enable = mkEnableOption "hyprpolkitagent service";
+  config = mkIf cfg.enable {
+    assertions = [
+      {
+        assertion = config.modules.desktop.hyprland.enable;
+        message = "hyprpolkitagent requires the hyprland desktop";
+      }
+      {
+        assertion = config.security.polkit.enable;
+        message = "hyprpolkitagent requires polkit to be enabled";
+      }
+    ];
+    environment.systemPackages = [ pkgs.hyprpolkitagent ];
+    systemd.user.services.hyprpolkitagent.wantedBy = [ "graphical-session.target" ];
+  };
+}
