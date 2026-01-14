b887a294db21c9999252a85d38b32b3376fa4f8c
kruziikrel13 <dev@michaelpetersen.io>
2026-01-14 13:41:50 +1000

    nvim: impl neovim config with extraPackages

--- modules/editors/nvim.nix
+++ modules/editors/nvim.nix
@@ -2,22 +2,54 @@
   lib,
   lib',
   config,
+  pkgs,
   neovim-nightly-overlay,
   ...
 }:
 let
-  inherit (lib'.options) mkBoolOpt;
+  inherit (lib'.options) mkOpt mkBoolOpt;
   cfg = config.modules.editors.nvim;
 in
 {
-  options.modules.editors.nvim.enable = mkBoolOpt (config.modules.editors.default == "nvim");
+  options.modules.editors.nvim = with lib.types; {
+    enable = mkBoolOpt (config.modules.editors.default == "nvim");
+    extraPackages = mkOpt (listOf package) [ ];
+    finalPackage = lib.mkOption {
+      type = package;
+      readOnly = true;
+    };
+  };
   config = lib.mkIf cfg.enable {
     nixpkgs.overlays = [ neovim-nightly-overlay.overlays.default ];
-    programs.neovim = {
-      enable = true;
-      defaultEditor = config.modules.editors.default == "nvim";
+    environment = {
+      systemPackages = [ cfg.finalPackage ];
+      shellAliases.vimdiff = "nvim -d";
+      variables = {
+        EDITOR = lib.mkOverride 900 "nvim";
+        VISUAL = lib.mkOverride 900 "nvim";
+      };
+      pathsToLink = [ "/share/nvim" ];
+    };
+    modules.editors.nvim.finalPackage = pkgs.wrapNeovimUnstable pkgs.neovim-unwrapped {
       viAlias = true;
       vimAlias = true;
+      waylandSupport = true;
+      wrapperArgs = with pkgs; [
+        "--suffix"
+        "PATH"
+        ":"
+        (lib.makeBinPath (
+          [
+            gcc
+            ripgrep
+            fzf
+            lazygit
+            tree-sitter
+            github-copilot-cli
+          ]
+          ++ cfg.extraPackages
+        ))
+      ];
     };
   };
 }
