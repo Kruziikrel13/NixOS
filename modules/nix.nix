12d014acbd20357f92b2e54ab7c351461222f9b7
kruziikrel13 <dev@michaelpetersen.io>
2026-01-14 11:42:23 +1000

    nix: migrate substituters to module system

--- modules/nix.nix
+++ modules/nix.nix
@@ -37,8 +37,25 @@
         http2 = true;
         warn-dirty = false;
         accept-flake-config = false;
+
+        substituters = [
+          "https://cache.nixos.org?priority=10"
+
+          "https://nix-community.cachix.org"
+          "https://nix-gaming.cachix.org"
+          "https://attic.xuyh0120.win/lantian"
+        ];
+
+        trusted-public-keys = [
+          "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+
+          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+          "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
+          "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
+        ];
       };
     };
+
   documentation = {
     nixos.enable = false;
     dev.enable = true;
