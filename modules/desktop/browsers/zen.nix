0ca40e30410bfe63a8183717aa691955d1afd374
kruziikrel13 <dev@michaelpetersen.io>
2026-01-15 18:24:28 +1000

    Revert "zen: define opts using programs.firefox"
    This reverts commit c823037d8901fbc58d6a70abd92a25ea18c71e38.

--- modules/desktop/browsers/zen.nix
+++ modules/desktop/browsers/zen.nix
@@ -9,7 +9,14 @@ let
   inherit (lib.options) mkEnableOption;
   inherit (lib.modules) mkIf;
   cfg = config.modules.desktop.browsers.zen;
-  package = zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.default;
+  zen = zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.zen-browser-unwrapped;
+  toPrefJson =
+    prefs:
+    lib.concatLines (
+      lib.mapAttrsToList (
+        name: value: ''lockPref(${lib.strings.toJSON name}, ${lib.strings.toJSON value});''
+      ) prefs
+    );
 in
 {
   options.modules.desktop.browsers.zen = {
@@ -17,42 +24,41 @@ in
   };
 
   config = mkIf cfg.enable {
-    programs.firefox = {
-      inherit package;
-      enable = true;
-      policies = {
-        DisableAppUpdate = true;
-        DisableTelemetry = true;
-        HardwareAcceleration = true;
-        NoDefaultBookmarks = false;
-        OfferToSaveLogins = false;
-        SearchEngines.Default = "ddg";
-        ExtensionSettings = {
-          "extension@tabliss-maintained" = {
-            install_url = "https://addons.mozilla.org/firefox/downloads/latest/tablissng/latest.xpi";
-            installation_mode = "force_installed";
-          };
-          # Ublock
-          "uBlock0@raymondhill.net" = {
-            install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
-            installation_mode = "normal_installed";
-            private_browsing = true;
-          };
-          # Bitwarden
-          "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
-            install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
-            installation_mode = "force_installed";
-            private_browsing = true;
-            default_area = "navbar";
-          };
-        };
-        preferences = {
+    user.packages = [
+      (pkgs.wrapFirefox zen {
+        extraPrefs = toPrefJson {
           "zen.urlbar.replace-newtab" = false;
           "zen.view.sidebar-expanded" = false;
           "zen.view.use-single-toolbar" = false;
           "zen.view.show-newtab-button-top" = false;
         };
-      };
-    };
+        extraPolicies = {
+          DisableAppUpdate = true;
+          DisableTelemetry = true;
+          NoDefaultBookmarks = false;
+          OfferToSaveLogins = false;
+          SearchEngines.Default = "ddg";
+          ExtensionSettings = {
+            "extension@tabliss-maintained" = {
+              install_url = "https://addons.mozilla.org/firefox/downloads/latest/tablissng/latest.xpi";
+              installation_mode = "force_installed";
+            };
+            # Ublock
+            "uBlock0@raymondhill.net" = {
+              install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+              installation_mode = "normal_installed";
+              private_browsing = true;
+            };
+            # Bitwarden
+            "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
+              install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
+              installation_mode = "force_installed";
+              private_browsing = true;
+              default_area = "navbar";
+            };
+          };
+        };
+      })
+    ];
   };
 }
