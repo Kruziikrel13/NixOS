a77dd74aa5da0e40b8caf9b7d8e33773a4deeec4
kruziikrel13 <dev@michaelpetersen.io>
2026-01-18 18:34:22 +1000

    services: impl hypridle

--- modules/services/hypridle.nix
+++ modules/services/hypridle.nix
@@ -2,16 +2,29 @@
   lib',
   lib,
   config,
+  options,
   ...
 }:
 let
-
-  inherit (lib'.options) mkBoolOpt;
+  inherit (lib) optionalString mkAliasDefinitions;
+  inherit (lib'.options) mkBoolOpt mkOpt;
   cfg = config.modules.services.hypridle;
+  hyprCfg = options.modules.desktop.hyprland.idle;
 in
 {
-  options.modules.services.hypridle.enable = mkBoolOpt false;
+  options.modules.services.hypridle = with lib.types; {
+    enable = mkBoolOpt (config.modules.desktop.hyprland.idle.time > 0);
+    # Aliased to modules.desktop.hyprland.idle;
+    time = mkOpt int 0;
+    autodpms = mkOpt int 0;
+    autosleep = mkOpt int 0;
+  };
   config = lib.mkIf cfg.enable {
+    modules.services.hypridle = {
+      time = mkAliasDefinitions hyprCfg.time;
+      autodpms = mkAliasDefinitions hyprCfg.autodpms;
+      autosleep = mkAliasDefinitions hyprCfg.autosleep;
+    };
     assertions = [
       {
         assertion = config.modules.desktop.hyprland.enable;
@@ -19,5 +32,30 @@ in
       }
     ];
     services.hypridle.enable = true;
+    environment.etc."xdg/hypr/hypridle.conf".text = ''
+      general:lock_cmd = ""
+      general:unlock_cmd = ""
+    ''
+    + optionalString (cfg.time > 0) ''
+      # Lock Session Listener
+      listener {
+        timeout = ${toString cfg.time}
+        on-timeout = "loginctl lock-session"
+      }
+    ''
+    + optionalString (cfg.autodpms > 0) ''
+      # DPMS Listener
+      listener {
+        timeout = ${toString cfg.autodpms}
+        on-timeout = "hyprctl dispatch dpms off"
+      }
+    ''
+    + optionalString (cfg.autosleep > 0) ''
+      # Sleep Listener
+      listener {
+        timeout = ${toString cfg.autosleep}
+        on-timeout = "systemctl suspend || loginctl suspend"
+      }
+    '';
   };
 }
