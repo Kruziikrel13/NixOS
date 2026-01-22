722351d483c7387af076ddbf4493612680f50b0a
kruziikrel13 <dev@michaelpetersen.io>
2026-01-22 11:04:53 +1000

    boot: ensure systemd-boot is enabled by default priority

--- modules/system/boot.nix
+++ modules/system/boot.nix
@@ -1,9 +1,11 @@
+{ lib, ... }:
 {
   console.earlySetup = true;
   boot.loader = {
     timeout = 5;
     efi.canTouchEfiVariables = true;
     systemd-boot = {
+      enable = lib.mkDefault true;
       editor = false;
       consoleMode = "max";
       configurationLimit = 10;
