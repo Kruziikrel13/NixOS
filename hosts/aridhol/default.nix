eff5aba416b3d4fc135044e82e5bcd60c61eade6
kruziikrel13 <dev@michaelpetersen.io>
2026-01-22 18:11:26 +1000

    aridhol: fixup drive mounts

--- hosts/aridhol/default.nix
+++ hosts/aridhol/default.nix
@@ -47,40 +47,34 @@
       git = {
         enable = true;
         email = "dev@michaelpetersen.io";
-        signingKey = "CC4C1D82D045B5DA";
+        # signingKey = "CC4C1D82D045B5DA";
       };
     };
   };
   boot.initrd.luks.devices."enc".device = "/dev/disk/by-uuid/a95aa5e1-a1e6-47be-b78f-663246bfd078";
   fileSystems = {
     "/" = {
-      device = "/dev/disk/by-uuid/158c17d8-d90d-443d-ab0d-8ebce12758db";
+      device = "/dev/disk/by-label/NIXOS";
       fsType = "btrfs";
-      options = [ "subvol=root" ];
-    };
-    "/nix" = {
-      device = "/dev/disk/by-uuid/158c17d8-d90d-443d-ab0d-8ebce12758db";
-      fsType = "btrfs";
-      options = [ "subvol=nix" ];
-    };
-    "/var/log" = {
-      device = "/dev/disk/by-uuid/158c17d8-d90d-443d-ab0d-8ebce12758db";
-      fsType = "btrfs";
-      options = [ "subvol=log" ];
     };
     "/home" = {
-      device = "/dev/disk/by-uuid/158c17d8-d90d-443d-ab0d-8ebce12758db";
+      device = "/dev/disk/by-label/NIXOS";
       fsType = "btrfs";
-      options = [ "subvol=home" ];
-    };
-    "/persist" = {
-      device = "/dev/disk/by-uuid/158c17d8-d90d-443d-ab0d-8ebce12758db";
-      fsType = "btrfs";
-      options = [ "subvol=persist" ];
     };
     "/boot" = {
-      device = "/dev/disk/by-uuid/1F99-8700";
+      device = "/dev/disk/by-label/BOOT";
       fsType = "vfat";
+    };
+    "/snapshots" = {
+      device = "/dev/disk/by-label/NIXOS";
+      fsType = "btrfs";
+      options = [
+        "subvol=@snapshots"
+        "compress=zstd:6"
+        "noexec"
+        "nosuid"
+        "sync"
+      ];
     };
   };
 
