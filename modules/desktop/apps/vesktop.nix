40161d618edd3f837a971fc9101c948fb23dbf25
kruziikrel13 <dev@michaelpetersen.io>
2026-01-29 08:23:26 +1000

    vesktop: configure xdph for screen sharing if enabled

--- modules/desktop/apps/vesktop.nix
+++ modules/desktop/apps/vesktop.nix
@@ -10,11 +10,22 @@ let
 in
 {
   options.modules.desktop.apps.vesktop.enable = mkBoolOpt false;
-  config = lib.mkIf config.modules.desktop.apps.vesktop.enable {
-    user.packages = [
-      (pkgs.vesktop.override {
-        withSystemVencord = true;
+  config = lib.mkIf config.modules.desktop.apps.vesktop.enable (
+    lib.mkMerge [
+      (lib.mkIf config.programs.hyprland.enable {
+        environment.etc."xdg/hypr/xdph.conf".text = ''
+          screencopy {
+            allow_token_by_default = true
+          }
+        '';
       })
-    ];
-  };
+      {
+        user.packages = [
+          (pkgs.vesktop.override {
+            withSystemVencord = true;
+          })
+        ];
+      }
+    ]
+  );
 }
