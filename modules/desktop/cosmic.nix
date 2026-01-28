822b4205214d678733d4f6b141766fa7f4a40b03
kruziikrel13 <dev@michaelpetersen.io>
2026-01-29 08:01:13 +1000

    cosmic: cleanup defs

--- modules/desktop/cosmic.nix
+++ modules/desktop/cosmic.nix
@@ -5,12 +5,7 @@
   ...
 }:
 let
-  inherit (lib)
-    attrValues
-    filter
-    length
-    ;
-  inherit (lib.modules) mkIf mkMerge;
+  inherit (lib.modules) mkIf;
   inherit (lib'.options) mkBoolOpt;
 in
 {
@@ -18,22 +13,8 @@ in
     enable = mkBoolOpt false;
   };
 
-  config = mkIf config.modules.desktop.cosmic.enable (mkMerge [
-    {
-      assertions = [
-        {
-          assertion =
-            !config.modules.desktop.cosmic.autoLogin
-            || (
-              length (
-                filter (desktop: desktop ? autoLogin && desktop.autoLogin) (attrValues config.modules.desktop)
-              ) <= 1
-            );
-          message = "Only one desktop module can have autoLogin enabled at a time. Niri has autoLogin enabled, but another desktop module also has it enabled.";
-        }
-      ];
-      services.displayManager.cosmic-greeter.enable = true;
-      services.desktopManager.cosmic.enable = true;
-    }
-  ]);
+  config = mkIf config.modules.desktop.cosmic.enable {
+    services.displayManager.cosmic-greeter.enable = true;
+    services.desktopManager.cosmic.enable = true;
+  };
 }
