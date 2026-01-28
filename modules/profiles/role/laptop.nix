1145acdbdc1da8a1c9db62dce1107d4e3121c723
kruziikrel13 <dev@michaelpetersen.io>
2026-01-29 08:00:36 +1000

    laptop: fixup tlp configuration settings

--- modules/profiles/role/laptop.nix
+++ modules/profiles/role/laptop.nix
@@ -30,16 +30,18 @@ mkIf (config.modules.profiles.role == "laptop") {
       enable = true;
       settings = {
         CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-        CPU_SCALING_GOVERNOR_ON_AC = "ondemand";
+        CPU_SCALING_GOVERNOR_ON_AC = "performance";
         CPU_MAX_PERF_ON_AC = 100;
         CPU_MAX_PERF_ON_BAT = 50;
+
+        CPU_BOOST_ON_AC = 1;
 
         # My laptop is always plugged in wherever I'm willing to use it, so I'll
         # value battery lifespan over runtime. Run `tlp fullcharge` to temporarily
         # force full charge.
         # @see https://linrunner.de/tlp/faq/battery.html#how-to-choose-good-battery-charge-thresholds
-        START_CHARGE_THRESH_BAT0 = 40;
-        STOP_CHARGE_THRESH_BAT0 = 80;
+        START_CHARGE_THRESH_BAT0 = 0;
+        STOP_CHARGE_THRESH_BAT0 = 1;
       };
     };
   };
