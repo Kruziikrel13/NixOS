981200d3c2c8e43ef1417e1c3abb533c490d0140
kruziikrel13 <dev@michaelpetersen.io>
2026-03-11 00:02:29 +1000

    protonvpn: update server config

--- modules/services/protonvpn.nix
+++ modules/services/protonvpn.nix
@@ -19,19 +19,26 @@ in
   config = mkIf cfg.enable {
     networking.wg-quick.interfaces."protonvpn" = {
       inherit (cfg) autostart;
-      dns = [ "10.2.0.1" ];
+      dns = [
+        "10.2.0.1"
+        "2a07:b944::2:1"
+      ];
       privateKeyFile = "/root/secrets/au.prv";
-      address = [ "10.2.0.2/32" ];
+      address = [
+        "10.2.0.2/32"
+        "2a07:b944::2:2/128"
+      ];
       listenPort = 51820;
 
       peers = [
         {
-          publicKey = "2dFWLSyohbnz02CORBKdh/bPh2PUqpfJISaWIN5/fHI=";
+          # AU226
+          publicKey = "hPKSC01LiQsP+1pzPm98CFZXqkESBuwqdmMe+4ujeEs=";
           allowedIPs = [
             "0.0.0.0/0"
             "::/0"
           ];
-          endpoint = "144.48.39.226:51820";
+          endpoint = "103.216.220.98:51820";
           persistentKeepalive = 25;
         }
       ];
