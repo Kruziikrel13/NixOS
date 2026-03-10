90f337222eea01dc0a0a657d5eb5a31af1d26423
kruziikrel13 <dev@michaelpetersen.io>
2026-03-10 12:19:42 +1000

    lib: additional attrs functions

--- lib/attrs.nix
+++ lib/attrs.nix
@@ -1,10 +1,52 @@
 { lib }:
-with builtins;
+let
+  inherit (lib.attrsets)
+    filterAttrs
+    mapAttrs
+    mapAttrs'
+    zipAttrsWith
+    isAttrs
+    ;
+  inherit (lib.lists)
+    head
+    tail
+    all
+    last
+    concatLists
+    isList
+    ;
+in
 {
   mapFilterAttrs =
     filter: predicate: attrs:
-    lib.filterAttrs predicate (builtins.mapAttrs filter attrs);
+    filterAttrs predicate (mapAttrs filter attrs);
   mapFilterAttrs' =
     filter: predicate: attrs:
-    lib.filterAttrs predicate (lib.mapAttrs' filter attrs);
+    filterAttrs predicate (mapAttrs' filter attrs);
+  filterMapAttrs =
+    predicate: filter: attrs:
+    (mapAttrs filter (filterAttrs predicate attrs));
+  filterMapAttrs' =
+    predicate: filter: attrs:
+    (mapAttrs' filter (filterAttrs predicate attrs));
+  # Unlike //, this will deeply merge attrsets (left > right).
+  # mergeAttrs' :: listOf attrs -> attrs
+  mergeAttrs' =
+    attrList:
+    let
+      f =
+        attrPath:
+        zipAttrsWith (
+          n: values:
+          if (tail values) == [ ] then
+            head values
+          else if all isList values then
+            concatLists values
+          else if all isAttrs values then
+            f (attrPath ++ [ n ]) values
+          else
+            last values
+        );
+    in
+    f [ ] attrList;
 }
