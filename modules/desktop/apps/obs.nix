9b3a165109c917abf4a27076c32fbe96f5c40815
kruziikrel13 <dev@michaelpetersen.io>
2026-03-16 17:11:31 +1000

    obs: formatting

--- modules/desktop/apps/obs.nix
+++ modules/desktop/apps/obs.nix
@@ -12,7 +12,7 @@ let
 in
 {
   options.modules.desktop.apps.obs.enable = mkBoolOpt false;
-  config = lib.mkIf cfg.enable {
+  config = mkIf cfg.enable {
     user.packages = [ pkgs.obs-studio ];
 
     environment.variables = mkIf config.modules.desktop.hyprland.enable {
