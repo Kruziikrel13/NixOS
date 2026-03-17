0d171f3720dfcc60ee8c52c51d3d1579c0e88083
kruziikrel13 <dev@michaelpetersen.io>
2026-03-17 12:08:21 +1000

    protonvpn: use config file

--- modules/services/protonvpn.nix
+++ modules/services/protonvpn.nix
@@ -6,42 +6,21 @@
 }:
 let
   inherit (lib.modules) mkIf;
-  inherit (self.lib.options) mkBoolOpt;
+  inherit (self.lib.options) mkBoolOpt mkOpt;
 
   cfg = config.modules.services.protonvpn;
 in
 {
-  options.modules.services.protonvpn = {
+  options.modules.services.protonvpn = with lib.types; {
     enable = mkBoolOpt false;
     autostart = mkBoolOpt true;
+    conf = mkOpt str "";
   };
 
   config = mkIf cfg.enable {
     networking.wg-quick.interfaces."protonvpn" = {
       inherit (cfg) autostart;
-      dns = [
-        "10.2.0.1"
-        "2a07:b944::2:1"
-      ];
-      privateKeyFile = "/root/secrets/au.prv";
-      address = [
-        "10.2.0.2/32"
-        "2a07:b944::2:2/128"
-      ];
-      listenPort = 51820;
-
-      peers = [
-        {
-          # AU226
-          publicKey = "hPKSC01LiQsP+1pzPm98CFZXqkESBuwqdmMe+4ujeEs=";
-          allowedIPs = [
-            "0.0.0.0/0"
-            "::/0"
-          ];
-          endpoint = "103.216.220.98:51820";
-          persistentKeepalive = 25;
-        }
-      ];
+      configFile = cfg.conf;
     };
   };
 }
