cdd6a35d79496c82989c231a37dcea224702a12a
kruziikrel13 <dev@michaelpetersen.io>
2026-04-13 16:26:53 +1000

    steam: modifications to various env vars

--- modules/desktop/apps/steam.nix
+++ modules/desktop/apps/steam.nix
@@ -27,9 +27,7 @@ in
           package = pkgs.steam.override {
             extraEnv = {
               PROTON_ENABLE_HDR = "1";
-              ENABLE_HDR_WSI = "1";
               PROTON_ENABLE_WAYLAND = "1";
-              DXVK_HDR = "1";
             };
           };
           extraCompatPackages = [ pkgs.proton-ge-bin ];
@@ -47,8 +45,8 @@ in
           enable = true;
           env = {
             WLR_RENDERER = "vulkan";
-            DXVK_HDR = "1";
             ENABLE_GAMESCOPE_WSI = "1";
+            PROTON_ENABLE_WAYLAND = "0";
           };
           args = [
             "--rt"
