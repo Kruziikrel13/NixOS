b8c23c127b1c9bd74e8274e26ead3159730daa96
kruziikrel13 <dev@michaelpetersen.io>
2026-04-17 12:26:27 +1000

    steam: enable fsr4 by default in proton

--- modules/desktop/apps/steam.nix
+++ modules/desktop/apps/steam.nix
@@ -28,6 +28,7 @@ in
             extraEnv = {
               PROTON_ENABLE_HDR = "1";
               PROTON_ENABLE_WAYLAND = "1";
+              PROTON_FSR4_UPGRADE = "1";
             };
           };
           extraCompatPackages = [ pkgs.proton-ge-bin ];
