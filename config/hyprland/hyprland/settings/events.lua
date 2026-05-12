5fb975650c87b6ef77a22993ec34240076e7b1a4
kruziikrel13 <dev@michaelpetersen.io>
2026-05-13 08:27:23 +1000

    hyprland: remove broken event

--- config/hyprland/hyprland/settings/events.lua
+++ config/hyprland/hyprland/settings/events.lua
@@ -3,7 +3,3 @@ hl.on("hyprland.start", function()
 	hl.exec_cmd("hyprctl setcursor" .. " " .. cursor.name .. " " .. cursor.size)
 	hl.exec_cmd("uwsm finalize")
 end)
-
-hl.on("window.move_to_workspace", function(w)
-	hl.dsp.window.center({ window = w.address })
-end)
