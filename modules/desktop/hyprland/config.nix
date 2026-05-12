61635775cd2998a0df5cb34af7e6a91a6bee8f21
kruziikrel13 <dev@michaelpetersen.io>
2026-05-12 14:36:32 +1000

    hyprland: map monitors declared with nix

--- modules/desktop/hyprland/config.nix
+++ modules/desktop/hyprland/config.nix
@@ -12,9 +12,10 @@ let
   inherit (lib.meta) getExe;
   inherit (cfg) monitors;
   inherit (lib.lists) findFirst;
-  primary = (findFirst (monitor: monitor.primary) { } monitors);
+  inherit (lib.generators) toLua;
+  primary = findFirst (monitor: monitor.primary) { } monitors;
 
-  conf = relativeToRoot "config/hypr";
+  conf = relativeToRoot "config/hyprland";
 in
 mkIf cfg.enable {
   environment.etc = {
@@ -23,28 +24,20 @@ mkIf cfg.enable {
     "xdg/hypr/hyprland/rules".source = "${conf}/hyprland/rules";
     "xdg/hypr/hyprland/keybinds".source = "${conf}/hyprland/keybinds";
     "xdg/hypr/hyprland/patches.lua".source = "${conf}/hyprland/patches.lua";
-    "xdg/hypr/hyprland/variables.lua".source = pkgs.replaceVars "${conf}/variables.lua.in" {
+    "xdg/hypr/hyprland/variables.lua".source = pkgs.replaceVars ./variables.lua.in {
       primary = primary.output;
-      monitors = ''
-        {
-               {
-                       output = "desc:AOC 24G1WG4 0x0004A33C",
-                       mode = "1920x1080@60.0",
-                       position = "-1920x345",
-               },
-               {
-                       output = "desc:GIGA-BYTE TECHNOLOGY CO. LTD. AORUS FI32U 21440B000115",
-                       mode = "3840x2160@144.00",
-                       position = "0x0",
-                       hdr = true,
-               },
-               {
-                       output = "desc:ViewSonic Corporation VX2758-C-MH V9M184500179",
-                       mode = "1920x1080@144.0",
-                       position = "3840x850",
-               },
-             }
-      '';
+      monitors = toLua { } (
+        map (mon: {
+          inherit (mon)
+            output
+            mode
+            position
+            scale
+            hdr
+            disable
+            ;
+        }) monitors
+      );
       runapp = getExe pkgs.runapp;
       terminal = getExe pkgs.ghostty;
       launcher = getExe pkgs.hyprlauncher;
