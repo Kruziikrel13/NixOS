1786bedd2eb2be2cc7bde525087c89b974407e18
kruziikrel13 <dev@michaelpetersen.io>
2026-05-14 07:26:40 +1000

    format

--- config/hyprland/hyprland/patches.lua
+++ config/hyprland/hyprland/patches.lua
@@ -1,6 +1,4 @@
 -- Ensure hardware cursors value is set to 2 when direct scanout is enabled
 if hl.get_config("render.direct_scanout") == 2 and hl.get_config("cursor.no_hardware_cursors") ~= 2 then
-	hl.config({
-		cursor = { no_hardware_cursors = 2 },
-	})
+	hl.config({ cursor = { no_hardware_cursors = 2 } })
 end
