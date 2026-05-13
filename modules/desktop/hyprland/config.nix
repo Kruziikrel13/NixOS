9dd316e26cd6af3a2390b30511e9d921037d712e
kruziikrel13 <dev@michaelpetersen.io>
2026-05-13 11:02:23 +1000

    hyprland: map lua files instead of hard declaring

--- modules/desktop/hyprland/config.nix
+++ modules/desktop/hyprland/config.nix
@@ -16,36 +16,44 @@ let
   primary = findFirst (monitor: monitor.primary) { } monitors;
 
   conf = relativeToRoot "config/hyprland";
+  fmap = [
+    "hyprland.lua"
+    "hyprland/patches.lua"
+    "hyprland/settings"
+    "hyprland/rules"
+  ];
 in
 mkIf cfg.enable {
-  environment.etc = {
-    "xdg/hypr/hyprland.lua".source = "${conf}/hyprland.lua";
-    "xdg/hypr/hyprland/settings".source = "${conf}/hyprland/settings";
-    "xdg/hypr/hyprland/rules".source = "${conf}/hyprland/rules";
-    "xdg/hypr/hyprland/keybinds".source = "${conf}/hyprland/keybinds";
-    "xdg/hypr/hyprland/patches.lua".source = "${conf}/hyprland/patches.lua";
-    "xdg/hypr/hyprland/variables.lua".source = pkgs.replaceVars ./variables.lua.in {
-      primary = if (primary ? output) then primary.output else "";
-      monitors = toLua { } (
-        map (mon: {
-          inherit (mon)
-            output
-            mode
-            position
-            scale
-            hdr
-            disable
-            ;
-        }) monitors
-      );
-      runapp = getExe pkgs.runapp;
-      terminal = getExe pkgs.ghostty;
-      launcher = getExe pkgs.hyprlauncher;
-      shutdown = getExe pkgs.hyprshutdown;
-      playerctl = getExe pkgs.playerctl;
-      wpctl = "${pkgs.wireplumber}/bin/wpctl";
-      cursorName = "Bibata-Modern-Classic-Hyprcursor";
-      cursorSize = 16;
+  environment.etc =
+    builtins.listToAttrs (
+      map (f: {
+        name = "xdg/hypr/${f}";
+        value.source = "${conf}/${f}";
+      }) fmap
+    )
+    // {
+      "xdg/hypr/hyprland/variables.lua".source = pkgs.replaceVars ./variables.lua.in {
+        primary = if (primary ? output) then primary.output else "";
+        monitors = toLua { } (
+          map (mon: {
+            inherit (mon)
+              output
+              mode
+              position
+              scale
+              hdr
+              disable
+              ;
+          }) monitors
+        );
+        runapp = getExe pkgs.runapp;
+        terminal = getExe pkgs.ghostty;
+        launcher = getExe pkgs.hyprlauncher;
+        shutdown = getExe pkgs.hyprshutdown;
+        playerctl = getExe pkgs.playerctl;
+        wpctl = "${pkgs.wireplumber}/bin/wpctl";
+        cursorName = "Bibata-Modern-Classic-Hyprcursor";
+        cursorSize = 16;
+      };
     };
-  };
 }
