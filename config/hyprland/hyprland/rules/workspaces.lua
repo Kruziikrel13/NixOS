895e574092ce6dbc0933708cecf93dc5f0a93982
kruziikrel13 <dev@michaelpetersen.io>
2026-05-14 11:51:29 +1000

    hyprland: create workspace module and map

--- config/hyprland/hyprland/rules/workspaces.lua
+++ config/hyprland/hyprland/rules/workspaces.lua
@@ -1,24 +1,46 @@
-local function create_workspace(name, monitor, key)
-	hl.workspace_rule({
-		workspace = "name:" .. name,
-		monitor = monitor,
-		layout = "monocle",
-		decorate = false,
-		no_rounding = true,
-		gaps_in = 0,
-		gaps_out = 0,
-	})
+local M = {}
 
-	hl.window_rule({
-		match = { tag = name },
-		workspace = "name:" .. name,
-	})
+local map = {
+	gaming = {
+		monitor = Hyprland.primary_monitor,
+		bind = "G",
+	},
+	chat = {
+		monitor = "desc:ViewSonic Corporation VX2758-C-MH V9M184500179",
+		bind = "C",
+	},
+	email = {
+		monitor = "desc:ViewSonic Corporation VX2758-C-MH V9M184500179",
+		bind = "E",
+	},
+}
 
-	hl.define_submap("workspaces", function()
-		hl.bind(key, hl.dsp.focus({ workspace = "name:" .. name }))
-	end)
+function M.workspaces()
+	local list = {}
+
+	for name, _ in pairs(map) do
+		table.insert(list, name)
+	end
+
+	return list
 end
 
-create_workspace("gaming", Hyprland.primary_monitor, "G")
-create_workspace("chat", "desc:ViewSonic Corporation VX2758-C-MH V9M184500179", "C")
-create_workspace("email", "desc:ViewSonic Corporation VX2758-C-MH V9M184500179", "E")
+function M.setup()
+	for key, workspace in pairs(map) do
+		hl.workspace_rule({
+			workspace = "name:" .. key,
+			monitor = workspace.monitor,
+			layout = "monocle",
+			decorate = false,
+			no_rounding = true,
+			gaps_in = 0,
+			gaps_out = 0,
+		})
+
+		hl.define_submap("workspaces", function()
+			hl.bind(workspace.bind, hl.dsp.focus({ workspace = "name:" .. key }))
+		end)
+	end
+end
+
+return M
